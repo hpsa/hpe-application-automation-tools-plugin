192620878bde65c484546e1887bb340f72aab3eb
Guller, Yuri <yuri.guller@hp.com>
2015-02-27 00:27:09 +0200

    some refactor in login logic in rest client, wip

--- jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/configuration/RestUtils.java
+++ jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/configuration/RestUtils.java
@@ -2,21 +2,43 @@ package com.hp.octane.plugins.jenkins.configuration;
 
 import org.apache.commons.httpclient.Cookie;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.auth.AuthPolicy;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.PutMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by gullery on 15/02/2015.
  */
 public class RestUtils {
 
-	private static Cookie[] cookies;
-	private static final String sessionXml = "<session-parameters><client-type>Octane Jenkins Plugin</client-type><time-out>6</time-out></session-parameters>";
+	private static HashMap<String, Cookie> cookies = new HashMap<String, Cookie>();
+	private static final String sessionXml = "<session-parameters><client-type>Octane Plugin (Jenkins)</client-type><time-out>6</time-out></session-parameters>";
 
-	private static HttpClient login(String url, String username, String password) throws Exception {
+	private static void storeCookies(Cookie[] cookiesArray) {
+		for (Cookie cookie : cookiesArray) {
+			cookies.put(cookie.getName(), cookie);
+		}
+	}
+
+	private static void applyCookies(HttpClient client) {
+		client.getState().clearCookies();
+		for (Cookie cookie : cookies.values()) {
+			client.getState().addCookie(cookie);
+		}
+	}
+
+	private static void login(String url, String username, String password) throws Exception {
 		int status;
 		HttpClient httpClient = new HttpClient();
+		List<String> authPrefs = new ArrayList<String>(2);
+		authPrefs.add(AuthPolicy.DIGEST);
+		authPrefs.add(AuthPolicy.BASIC);
+		httpClient.getParams().setParameter(AuthPolicy.AUTH_SCHEME_PRIORITY, authPrefs);
 
 		//  LWSSO login
 		PostMethod post = new PostMethod(url + "/qcbin/authentication-point/alm-authenticate");
@@ -24,39 +46,35 @@ public class RestUtils {
 				"<alm-authentication><user>" + username + "</user><password>" + password + "</password></alm-authentication>",
 				"application/xml",
 				"UTF-8"));
+		post.addRequestHeader("Content-type", "application/xml; charset=utf-8");
 
 		status = httpClient.executeMethod(post);
-		System.out.println(status);
-		cookies = httpClient.getState().getCookies();
+		System.out.println("Initial: " + status);
+		storeCookies(httpClient.getState().getCookies());
 
 		//  QC Session
 		post = new PostMethod(url + "/qcbin/rest/site-session");
 		post.setRequestEntity(new StringRequestEntity(sessionXml, "application/xml", "UTF-8"));
-		httpClient.getState().addCookies(cookies);
+		applyCookies(httpClient);
 
 		status = httpClient.executeMethod(post);
-		cookies = httpClient.getState().getCookies();
-		System.out.println(status);
-		return httpClient;
+		System.out.println("Session: " + status);
+		storeCookies(httpClient.getState().getCookies());
 	}
 
 	public static int put(String url, String path, String username, String password, String body) throws Exception {
 		int status;
-		HttpClient httpClient;
-		if (cookies == null) {
-			httpClient = login(url, username, password);
-		} else {
-			httpClient = new HttpClient();
-			httpClient.getState().addCookies(cookies);
+		if (cookies.size() == 0) {
+			login(url, username, password);
 		}
 
+		HttpClient httpClient = new HttpClient();
+		applyCookies(httpClient);
+
 		PutMethod putMethod = new PutMethod(url + path);
+		putMethod.setRequestHeader("X-XSRF-TOKEN", cookies.get("XSRF-TOKEN").getValue());
 		putMethod.setRequestEntity(new StringRequestEntity(body, "application/json", "UTF-8"));
-		for (Cookie c : cookies) {
-			if (c.getName().equals("XSRF-TOKEN")) {
-				putMethod.setRequestHeader("X-XSRF-TOKEN", c.getValue());
-			}
-		}
+
 		status = httpClient.executeMethod(putMethod);
 
 		if (status == 401) {
