dfb716bde549b88df001552bb6de597b7503ea3c
Vojtech Janota <vojtech.janota@hp.com>
2015-05-26 11:05:12 +0200

    defect #1563: detect module when executing maven using shell step

--- jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/tests/maven/AbstractMavenModuleDetection.java
+++ jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/tests/maven/AbstractMavenModuleDetection.java
@@ -13,7 +13,7 @@ import java.util.List;
 public abstract class AbstractMavenModuleDetection implements ModuleDetection {
 
     protected FilePath rootDir;
-    private List<FilePath> pomDirs;
+    protected List<FilePath> pomDirs;
 
     public AbstractMavenModuleDetection(AbstractBuild build) {
         rootDir = build.getWorkspace();
