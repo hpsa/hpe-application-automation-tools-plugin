8540f96e69c864584fa47e9a8544b132a1ac20f2
David Kalivoda <david.kalivoda@hp.com>
2015-07-09 14:00:20 +0200

    user story #5948, #5950: auto-framework detection

---
+++ jenkins/plugin/src/test/java/com/hp/octane/plugins/jenkins/tests/detection/ResultFieldsXmlReader.java
@@ -0,0 +1,61 @@
+package com.hp.octane.plugins.jenkins.tests.detection;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.Reader;
+
+public class ResultFieldsXmlReader {
+
+    private Reader input;
+    private XMLEventReader eventReader;
+    private ResultFields resultFields;
+
+    private boolean fieldsElement = false;
+
+    public ResultFieldsXmlReader(Reader input) throws XMLStreamException {
+        this.input = input;
+        eventReader = XMLInputFactory.newInstance().createXMLEventReader(input);
+        resultFields = new ResultFields();
+    }
+
+    public ResultFields readTestFields() {
+        try {
+            while (eventReader.hasNext()) {
+                XMLEvent event = eventReader.nextEvent();
+                if (event instanceof StartElement) {
+                    StartElement element = (StartElement) event;
+                    String localName = element.getName().getLocalPart();
+                    if ("fields".equals(localName)) {
+                        fieldsElement = true;
+                    }
+                    if ("field".equals(localName)) {
+                        if (!fieldsElement) {
+                            Assert.fail("<Field> element found, but surrounding element '<fields>' is missing in the XML file");
+                        }
+                        String type = element.getAttributeByName(new QName("type")).getValue();
+                        String value = element.getAttributeByName(new QName("value")).getValue();
+                        if (type.equals("Framework")) {
+                            resultFields.setFramework(value);
+                        } else if (type.equals("Testing Tool")) {
+                            resultFields.setTestingTool(value);
+                        } else if (type.equals("Test Level")) {
+                            resultFields.setTestLevel(value);
+                        }
+                    }
+                }
+            }
+            IOUtils.closeQuietly(input);
+            eventReader.close();
+        } catch (XMLStreamException e){
+            throw new RuntimeException(e);
+        }
+        return resultFields;
+    }
+}
