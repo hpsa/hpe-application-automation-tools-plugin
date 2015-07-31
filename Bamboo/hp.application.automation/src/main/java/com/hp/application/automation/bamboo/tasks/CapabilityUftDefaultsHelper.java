df2e3f85d9b0114c20a06382a900334ff2cf534f
Sergey Chernikov <sergey.chernikov@hp.com>
2015-07-31 15:34:24 +0300

    Detect UFT capability and require it in File System task

--- Bamboo/hp.application.automation/src/main/java/com/hp/application/automation/bamboo/tasks/CapabilityUftDefaultsHelper.java
+++ Bamboo/hp.application.automation/src/main/java/com/hp/application/automation/bamboo/tasks/CapabilityUftDefaultsHelper.java
@@ -1,11 +1,10 @@
 package com.hp.application.automation.bamboo.tasks;
 
-import com.atlassian.bamboo.v2.build.agent.capability.AbstractFileCapabilityDefaultsHelper;
 import com.atlassian.bamboo.v2.build.agent.capability.CapabilityDefaultsHelper;
 import com.atlassian.bamboo.v2.build.agent.capability.CapabilityImpl;
 import com.atlassian.bamboo.v2.build.agent.capability.CapabilitySet;
 import com.atlassian.util.concurrent.NotNull;
-import com.hp.application.automation.tools.common.SSEException;
+import com.hp.application.automation.tools.common.StringUtils;
 
 import java.io.File;
 
@@ -14,20 +13,43 @@ import java.io.File;
  */
 public class CapabilityUftDefaultsHelper implements CapabilityDefaultsHelper {
 
+    public static final String CAPABILITY_HP_ROOT = CapabilityDefaultsHelper.CAPABILITY_BUILDER_PREFIX + ".HP";
+    public static final String CAPABILITY_UFT = CAPABILITY_HP_ROOT + ".HP Unified Functional Testing";
+
     @NotNull
     @Override
     public CapabilitySet addDefaultCapabilities(CapabilitySet capabilitySet) {
-        if(isUftInstalled()) {
-            CapabilityImpl capability = new CapabilityImpl("UFT", "installed");
+        String uftPath = getUftExePath();
+        if(!StringUtils.isNullOrEmpty(uftPath)) {
+            CapabilityImpl capability = new CapabilityImpl(CAPABILITY_UFT, uftPath);
             capabilitySet.addCapability(capability);
+        }
+        else
+        {
+            capabilitySet.removeCapability(CAPABILITY_UFT);
         }
         return capabilitySet;
     }
 
-    private boolean isUftInstalled() {
-        SSEException e;
-        String installPath = WindowsRegistry.readRegistry("HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Mercury Interactive\\QuickTest Professional\\CurrentVersion", "QuickTest Professional");
+    private static final String uftRegistryKey = "SOFTWARE\\Mercury Interactive\\QuickTest Professional\\CurrentVersion";
+    private static final String uftRegistryValue = "QuickTest Professional";
+    private static final String uftExeName = "bin\\UFT.exe";
+    private static String getUftExePath()
+    {
+        String installPath = WindowsRegistry.readHKLMString(uftRegistryKey, uftRegistryValue);
+        if(StringUtils.isNullOrEmpty(installPath))
+        {
+            return "";
+        }
         File f = new File(installPath);
-        return f.exists() && f.isDirectory();
+        if(f.exists() && f.isDirectory())
+        {
+            f = new File(f, uftExeName);
+            if(f.exists() && f.isFile())
+            {
+                return f.getAbsolutePath();
+            }
+        }
+        return "";
     }
 }
