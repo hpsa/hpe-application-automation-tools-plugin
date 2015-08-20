c29a90269fe5022b894c11b688ffa5f85d49f44b
sadea <almog@hp.com>
2015-08-20 14:05:40 +0300

    fix history data

--- jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/actions/ProjectActions.java
+++ jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/actions/ProjectActions.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA.
@@ -66,7 +67,8 @@ public class ProjectActions extends TransientProjectActionFactory {
                     SCMData smData = SCMProcessors
                             .getAppropriate(abstractBuild.getProject().getScm().getClass().getName())
                             .getSCMChanges(abstractBuild);
-                    buildHistory.addBuild(abstractBuild.getResult().toString(), String.valueOf(abstractBuild.getNumber()), abstractBuild.getTimestampString(), String.valueOf(abstractBuild.getStartTimeInMillis()), String.valueOf(abstractBuild.getDuration()), smData);
+                    Set<User> users = abstractBuild.getCulprits();
+                    buildHistory.addBuild(abstractBuild.getResult().toString(), String.valueOf(abstractBuild.getNumber()), abstractBuild.getTimestampString(), String.valueOf(abstractBuild.getStartTimeInMillis()), String.valueOf(abstractBuild.getDuration()), smData, users);
                 }
             }
             SCMData smData = null;
@@ -75,23 +77,24 @@ public class ProjectActions extends TransientProjectActionFactory {
                 smData = SCMProcessors
                         .getAppropriate(lastSuccessfulBuild.getProject().getScm().getClass().getName())
                         .getSCMChanges(lastSuccessfulBuild);
-                buildHistory.addLastSuccesfullBuild(lastSuccessfulBuild.getResult().toString(), String.valueOf(lastSuccessfulBuild.getNumber()), lastSuccessfulBuild.getTimestampString(), String.valueOf(lastSuccessfulBuild.getStartTimeInMillis()), String.valueOf(lastSuccessfulBuild.getDuration()), smData);
+                Set<User> users = lastSuccessfulBuild.getCulprits();
+                buildHistory.addLastSuccesfullBuild(lastSuccessfulBuild.getResult().toString(), String.valueOf(lastSuccessfulBuild.getNumber()), lastSuccessfulBuild.getTimestampString(), String.valueOf(lastSuccessfulBuild.getStartTimeInMillis()), String.valueOf(lastSuccessfulBuild.getDuration()), smData, users);
             }
             AbstractBuild lastBuild = (AbstractBuild) project.getLastBuild();
             if (lastBuild != null) {
                 smData = SCMProcessors
                         .getAppropriate(lastBuild.getProject().getScm().getClass().getName())
                         .getSCMChanges(lastBuild);
+                Set<User> users = lastBuild.getCulprits();
                 if (lastBuild.getResult() == null) {
-                    buildHistory.addLastBuild("building", String.valueOf(lastBuild.getNumber()), lastBuild.getTimestampString(), String.valueOf(lastBuild.getStartTimeInMillis()), String.valueOf(lastBuild.getDuration()), smData);
+                    buildHistory.addLastBuild("building", String.valueOf(lastBuild.getNumber()), lastBuild.getTimestampString(), String.valueOf(lastBuild.getStartTimeInMillis()), String.valueOf(lastBuild.getDuration()), smData, users);
                 } else {
-                    buildHistory.addLastBuild(lastBuild.getResult().toString(), String.valueOf(lastBuild.getNumber()), lastBuild.getTimestampString(), String.valueOf(lastBuild.getStartTimeInMillis()), String.valueOf(lastBuild.getDuration()), smData);
+                    buildHistory.addLastBuild(lastBuild.getResult().toString(), String.valueOf(lastBuild.getNumber()), lastBuild.getTimestampString(), String.valueOf(lastBuild.getStartTimeInMillis()), String.valueOf(lastBuild.getDuration()), smData, users);
                 }
 
             }
             res.serveExposedBean(req, buildHistory, Flavor.JSON);
         }
-
 
         public void doRun(StaplerRequest req, StaplerResponse res) throws IOException, ServletException {
             //SecurityContext context = ACL.impersonate(User.get("gullerya").impersonate());
