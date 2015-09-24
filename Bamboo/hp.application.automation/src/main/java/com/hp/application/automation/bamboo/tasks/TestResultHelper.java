2fb48c20963e6aa662f4067fffe32f7d535d8a05
Alexey Shkvarchenko <alex.shkvarchenko@hp.com>
2015-09-24 15:46:41 +0300

    Added copyright notice

--- Bamboo/hp.application.automation/src/main/java/com/hp/application/automation/bamboo/tasks/TestResultHelper.java
+++ Bamboo/hp.application.automation/src/main/java/com/hp/application/automation/bamboo/tasks/TestResultHelper.java
@@ -1,3 +1,24 @@
+/**
+ © Copyright 2015 Hewlett Packard Enterprise Development LP
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
 package com.hp.application.automation.bamboo.tasks;
 
 import com.atlassian.bamboo.build.logger.BuildLogger;
@@ -11,15 +32,11 @@ import com.hp.application.automation.tools.common.result.model.junit.Testsuite;
 import com.hp.application.automation.tools.common.result.model.junit.Testsuites;
 import com.hp.application.automation.tools.common.sdk.DirectoryZipHelper;
 import org.apache.commons.lang.StringUtils;
-
 import javax.xml.bind.JAXBException;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-/**
- * Created by dsinelnikov on 7/31/2015.
- */
 public final class TestResultHelper
 {
     private static final String TEST_REPORT_FILE_PATTERNS = "*.xml";
