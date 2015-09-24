83574547cc78e4b71e3e9399d751d3d3047fcee5
Alexey Shkvarchenko <alex.shkvarchenko@hp.com>
2015-09-24 16:57:34 +0300

    Added copyright notice to common, removed LICENSE.txt, README.txt from Bamboo

--- common/src/main/java/com/hp/application/automation/tools/common/rest/RestClient.java
+++ common/src/main/java/com/hp/application/automation/tools/common/rest/RestClient.java
@@ -1,3 +1,25 @@
+/**
+ © Copyright 2015 Hewlett Packard Enterprise Development LP
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
 package com.hp.application.automation.tools.common.rest;
 
 import com.hp.application.automation.tools.common.SSEException;
@@ -46,26 +68,22 @@ public class RestClient implements Client {
                         String.format("projects/%s", project));
         _webuiPrefix = getPrefixUrl("webui/alm", domain, project);
     }
-    
-    @Override
+
     public String build(String suffix) {
         
         return String.format("%1$s%2$s", _serverUrl, suffix);
     }
-    
-    @Override
+
     public String buildRestRequest(String suffix) {
         
         return String.format("%1$s/%2$s", _restPrefix, suffix);
     }
-    
-    @Override
+
     public String buildWebUIRequest(String suffix) {
         
         return String.format("%1$s/%2$s", _webuiPrefix, suffix);
     }
-    
-    @Override
+
     public Response httpGet(
             String url,
             String queryString,
@@ -81,8 +99,7 @@ public class RestClient implements Client {
         
         return ret;
     }
-    
-    @Override
+
     public Response httpPost(
             String url,
             byte[] data,
@@ -114,8 +131,7 @@ public class RestClient implements Client {
         
         return ret;
     }
-    
-    @Override
+
     public String getServerUrl() {
         
         return _serverUrl;
@@ -295,8 +311,7 @@ public class RestClient implements Client {
         
         return ret.toString();
     }
-    
-    @Override
+
     public String getUsername() {
         
         return _username;
