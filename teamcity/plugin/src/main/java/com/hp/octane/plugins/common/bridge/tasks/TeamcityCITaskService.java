2255a93519b44971da045c2fe41ac938dbf03445
linsha <DeepPurple1974>
2016-01-17 11:07:12 +0200

    Refactor OPB Rest Client

--- teamcity/plugin/src/main/java/com/hp/octane/plugins/common/bridge/tasks/TeamcityCITaskService.java
+++ teamcity/plugin/src/main/java/com/hp/octane/plugins/common/bridge/tasks/TeamcityCITaskService.java
@@ -1,19 +1,25 @@
 package com.hp.octane.plugins.common.bridge.tasks;
 
+import com.hp.octane.plugins.jetbrains.teamcity.NGAPlugin;
 import com.hp.octane.plugins.jetbrains.teamcity.actions.StatusActionController;
+import com.hp.octane.plugins.jetbrains.teamcity.factories.ModelFactory;
+import com.hp.octane.plugins.jetbrains.teamcity.factories.TeamCityModelFactory;
 import com.hp.octane.plugins.jetbrains.teamcity.model.api.ProjectsList;
 import com.hp.octane.plugins.jetbrains.teamcity.utils.Utils;
+import jetbrains.buildServer.serverSide.ProjectManager;
+import jetbrains.buildServer.serverSide.SBuildServer;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by linsha on 07/01/2016.
  */
 public class TeamcityCITaskService implements CITaskService{
+
     @Override
     public String getProjects(boolean withParameters) {
-//        ProjectsList projectsList = modelFactory.CreateProjectList();
-//        Utils.updateResponse(projectsList,request,response);
-        return null;
-
+        ModelFactory modelFactory = new TeamCityModelFactory(NGAPlugin.getInstance().getProjectManager());
+        ProjectsList projectsList = modelFactory.CreateProjectList();
+        return Utils.jacksonRendering(projectsList);
     }
 
     @Override
--- teamcity/plugin/src/main/java/com/hp/octane/plugins/jetbrains/teamcity/NGAPlugin.java
+++ teamcity/plugin/src/main/java/com/hp/octane/plugins/jetbrains/teamcity/NGAPlugin.java
@@ -23,10 +23,10 @@ import java.util.Date;
 import java.util.UUID;
 import java.util.logging.Logger;
 
-public class PluginRouter implements ServerExtension {
-    public static final String PLUGIN_NAME = PluginRouter.class.getSimpleName().toLowerCase();
-    private static final Logger logger = Logger.getLogger(PluginRouter.class.getName());
-    private SBuildServer sBuildServer;
+public class NGAPlugin implements ServerExtension {
+    public static final String PLUGIN_NAME = NGAPlugin.class.getSimpleName().toLowerCase();
+    private static final Logger logger = Logger.getLogger(NGAPlugin.class.getName());
+
     private String identity;
     private Long identityFrom;
 
@@ -34,31 +34,56 @@ public class PluginRouter implements ServerExtension {
     private String location="http://localhost:8080";
     private final String PLUGIN_TYPE = "HPE_TEAMCITY_PLUGIN";
 
-    public PluginRouter(SBuildServer server,
-                        ProjectManager projectManager,
-                        BuildTypeResponsibilityFacade responsibilityFacade,
-                        WebControllerManager webControllerManager) {
+    private SBuildServer sBuildServer;
+    private ProjectManager projectManager;
+    private BuildTypeResponsibilityFacade responsibilityFacade;
+    private static NGAPlugin plugin;
+    public NGAPlugin(SBuildServer sBuildServer,
+                     ProjectManager projectManager,
+                     BuildTypeResponsibilityFacade responsibilityFacade,
+                     WebControllerManager webControllerManager) {
         logger.info("Init HPE MQM CI Plugin");
-        this.sBuildServer = server;
-        server.registerExtension(ServerExtension.class, PLUGIN_NAME, this);
+        sBuildServer.registerExtension(ServerExtension.class, PLUGIN_NAME, this);
+        this.plugin =  this;
+        this.sBuildServer = sBuildServer;
+        this.projectManager = projectManager;
+        this.responsibilityFacade = responsibilityFacade;
 //        server.addListener(new BuildEventListener());
-        ModelFactory modelFactory = new TeamCityModelFactory(projectManager);
-
-
-        webControllerManager.registerController("/octane/jobs/**",
-                new PluginActionsController(server, projectManager, responsibilityFacade,modelFactory));
-
-        webControllerManager.registerController("/octane/snapshot/**",
-                new BuildActionsController(server, projectManager, responsibilityFacade,modelFactory));
-        
-        webControllerManager.registerController("/octane/structure/**",
-                new ProjectActionsController(server, projectManager, responsibilityFacade,modelFactory));
-        webControllerManager.registerController("/octane/status/**",
-                new StatusActionController(server, projectManager, responsibilityFacade));
-        initiPlugin();
+        registerControllers(webControllerManager, projectManager, sBuildServer);
+        initOPB();
     }
 
-    private void initiPlugin() {
+    public SBuildServer getsBuildServer() {
+        return sBuildServer;
+    }
+
+    public ProjectManager getProjectManager() {
+        return projectManager;
+    }
+
+    public BuildTypeResponsibilityFacade getResponsibilityFacade() {
+        return responsibilityFacade;
+    }
+
+    public static NGAPlugin getInstance() {
+        return plugin;
+    }
+
+    private void registerControllers(WebControllerManager webControllerManager, ProjectManager projectManager,SBuildServer sBuildServer) {
+        ModelFactory modelFactory = new TeamCityModelFactory(projectManager);
+        webControllerManager.registerController("/octane/jobs/**",
+                new PluginActionsController(sBuildServer, projectManager, responsibilityFacade,modelFactory));
+
+        webControllerManager.registerController("/octane/snapshot/**",
+                new BuildActionsController(sBuildServer, projectManager, responsibilityFacade,modelFactory));
+
+        webControllerManager.registerController("/octane/structure/**",
+                new ProjectActionsController(sBuildServer, projectManager, responsibilityFacade,modelFactory));
+        webControllerManager.registerController("/octane/status/**",
+                new StatusActionController(sBuildServer, projectManager, responsibilityFacade));
+    }
+
+    private void initOPB() {
         if (StringUtils.isEmpty(DummyPluginConfiguration.identity)) {
             DummyPluginConfiguration.identity = UUID.randomUUID().toString();
         }
