fd67737437b8368d2f24e6e359dec51410cb39c5
kashbi <sami.kashbi@hp.com>
2016-03-03 17:06:14 +0200

    Fixing #4442 JI: Permission issue - cannot create pipeline when anonymous cannot list jobs

--- jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/CIJenkinsServicesImpl.java
+++ jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/CIJenkinsServicesImpl.java
@@ -111,68 +111,69 @@ public class CIJenkinsServicesImpl implements CIPluginServices {
         return result;
     }
 
-	@Override
-	public CIJobsList getJobsList(boolean includeParameters) {
-
-		CIJobsList result = dtoFactory.newDTO(CIJobsList.class);
-		CIJobMetadata tmpConfig;
-		AbstractProject tmpProject;
-		List<CIJobMetadata> list = new ArrayList<CIJobMetadata>();
+    @Override
+    public CIJobsList getJobsList(boolean includeParameters) {
+        SecurityContext securityContext = startImpersonation();
+        CIJobsList result = dtoFactory.newDTO(CIJobsList.class);
+        CIJobMetadata tmpConfig;
+        AbstractProject tmpProject;
+        List<CIJobMetadata> list = new ArrayList<CIJobMetadata>();
         try {
-        boolean hasReadPermission = Jenkins.getInstance().hasPermission(Item.READ);
-        if (!hasReadPermission){
-            stopImpersonation(securityContext);
-            throw new PermissionException(403);
-        }
-		List<String> itemNames = (List<String>) Jenkins.getInstance().getTopLevelItemNames();
-		for (String name : itemNames) {
-			if (Jenkins.getInstance().getItem(name) instanceof AbstractProject) {
-				tmpProject = (AbstractProject) Jenkins.getInstance().getItem(name);
-				tmpConfig = dtoFactory.newDTO(CIJobMetadata.class);
-				tmpConfig.setName(name);
-				tmpConfig.setCiId(name);
-				if (includeParameters) {
-					List<ParameterConfig> tmpList = ParameterProcessors.getConfigs(tmpProject);
-					List<com.hp.nga.integrations.dto.parameters.ParameterConfig> configs = new ArrayList<com.hp.nga.integrations.dto.parameters.ParameterConfig>();
-					for (ParameterConfig pc : tmpList) {
-						configs.add(new com.hp.nga.integrations.dto.parameters.ParameterConfig(
-								pc.getType(),
-								pc.getName(),
-								pc.getDescription(),
-								pc.getDefaultValue(),
-								pc.getChoices() == null ? null : pc.getChoices()
-						));
-					}
-					tmpConfig.setParameters(configs.toArray(new com.hp.nga.integrations.dto.parameters.ParameterConfig[configs.size()]));
-				}
-				list.add(tmpConfig);
-			} else {
-				logger.info("item '" + name + "' is not of supported type");
-			}
-		}
-		result.setJobs(list.toArray(new CIJobMetadata[list.size()]));
-        stopImpersonation(securityContext);
-            catch (AccessDeniedException e) {
+            boolean hasReadPermission = Jenkins.getInstance().hasPermission(Item.READ);
+            if (!hasReadPermission) {
                 stopImpersonation(securityContext);
                 throw new PermissionException(403);
             }
-		return result;
-	}
+            List<String> itemNames = (List<String>) Jenkins.getInstance().getTopLevelItemNames();
+            for (String name : itemNames) {
+                if (Jenkins.getInstance().getItem(name) instanceof AbstractProject) {
+                    tmpProject = (AbstractProject) Jenkins.getInstance().getItem(name);
+                    tmpConfig = dtoFactory.newDTO(CIJobMetadata.class);
+                    tmpConfig.setName(name);
+                    tmpConfig.setCiId(name);
+                    if (includeParameters) {
+                        List<ParameterConfig> tmpList = ParameterProcessors.getConfigs(tmpProject);
+                        List<com.hp.nga.integrations.dto.parameters.ParameterConfig> configs = new ArrayList<com.hp.nga.integrations.dto.parameters.ParameterConfig>();
+                        for (ParameterConfig pc : tmpList) {
+                            configs.add(new com.hp.nga.integrations.dto.parameters.ParameterConfig(
+                                    pc.getType(),
+                                    pc.getName(),
+                                    pc.getDescription(),
+                                    pc.getDefaultValue(),
+                                    pc.getChoices() == null ? null : pc.getChoices()
+                            ));
+                        }
+                        tmpConfig.setParameters(configs.toArray(new com.hp.nga.integrations.dto.parameters.ParameterConfig[configs.size()]));
+                    }
+                    list.add(tmpConfig);
+                } else {
+                    logger.info("item '" + name + "' is not of supported type");
+                }
+            }
+            result.setJobs(list.toArray(new CIJobMetadata[list.size()]));
+            stopImpersonation(securityContext);
+        } catch (AccessDeniedException e) {
+            stopImpersonation(securityContext);
+            throw new PermissionException(403);
+        }
+        return result;
+    }
 
     @Override
     public PipelineNode getPipeline(String rootCIJobId) {
         PipelineNode result;
-        SecurityContext securityContext=startImpersonation();
+        SecurityContext securityContext = startImpersonation();
         boolean hasRead = Jenkins.getInstance().hasPermission(Item.READ);
-        if (!hasRead){
+        if (!hasRead) {
+            stopImpersonation(securityContext);
             throw new PermissionException(403);
         }
         AbstractProject project = getJobByRefId(rootCIJobId);
         if (project != null) {
-            result =  ModelFactory.createStructureItem(project);
+            result = ModelFactory.createStructureItem(project);
             stopImpersonation(securityContext);
             return result;
-        }else{
+        } else {
             //todo: check error message(s)
             logger.warning("Failed to get project from jobRefId: '" + rootCIJobId + "' check plugin user Job Read/Overall Read permissions / project name");
             stopImpersonation(securityContext);
@@ -182,7 +183,7 @@ public class CIJenkinsServicesImpl implements CIPluginServices {
 
     private SecurityContext startImpersonation() {
         String user = Jenkins.getInstance().getPlugin(OctanePlugin.class).getImpersonatedUser();
-        SecurityContext originalContext=null;
+        SecurityContext originalContext = null;
         if (user != null && !user.equalsIgnoreCase("")) {
             User jenkinsUser = User.get(user, false);
             if (jenkinsUser != null) {
@@ -207,11 +208,11 @@ public class CIJenkinsServicesImpl implements CIPluginServices {
 
     @Override
     public void runPipeline(String ciJobId, String originalBody) {
-        SecurityContext securityContext=startImpersonation();
+        SecurityContext securityContext = startImpersonation();
         AbstractProject project = getJobByRefId(ciJobId);
         if (project != null) {
             boolean hasBuildPermission = project.hasPermission(Item.BUILD);
-            if (!hasBuildPermission){
+            if (!hasBuildPermission) {
                 stopImpersonation(securityContext);
                 throw new PermissionException(403);
             }
@@ -225,7 +226,7 @@ public class CIJenkinsServicesImpl implements CIPluginServices {
 
     @Override
     public SnapshotNode getSnapshotLatest(String ciJobId, boolean subTree) {
-        SecurityContext securityContext=startImpersonation();
+        SecurityContext securityContext = startImpersonation();
         SnapshotNode result = null;
         AbstractProject project = getJobByRefId(ciJobId);
         if (project != null) {
