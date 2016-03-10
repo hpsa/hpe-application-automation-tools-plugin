a31db9d6a2f307cf5a3ce948702a1d86894ce429
Guller Yuri <yuri.guller@hp.com>
2016-03-10 14:42:04 +0200

    tech: porting team city plugin to new rest client

--- teamcity/plugin/src/main/java/com/hp/octane/plugins/jetbrains/teamcity/TeamCityPluginServicesImpl.java
+++ teamcity/plugin/src/main/java/com/hp/octane/plugins/jetbrains/teamcity/TeamCityPluginServicesImpl.java
@@ -15,6 +15,9 @@ import com.hp.octane.plugins.jetbrains.teamcity.configuration.NGAConfig;
 import com.hp.octane.plugins.jetbrains.teamcity.factories.ModelFactory;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
@@ -25,6 +28,7 @@ import java.util.logging.Logger;
 
 public class TeamCityPluginServicesImpl implements CIPluginServices {
 	private static final Logger logger = Logger.getLogger(TeamCityPluginServicesImpl.class.getName());
+	private static final DTOFactory dtoFactory = DTOFactory.getInstance();
 	private static final String pluginVersion = "9.1.5";
 
 	@Override
@@ -34,7 +38,7 @@ public class TeamCityPluginServicesImpl implements CIPluginServices {
 			serverUrl = serverUrl.substring(0, serverUrl.length() - 1);
 		}
 
-		return DTOFactory.getInstance().newDTO(CIServerInfo.class)
+		return dtoFactory.newDTO(CIServerInfo.class)
 				.setInstanceId(NGAPlugin.getInstance().getConfig().getIdentity())
 				.setInstanceIdFrom(NGAPlugin.getInstance().getConfig().getIdentityFromAsLong())
 				.setSendingTime(System.currentTimeMillis())
@@ -45,7 +49,7 @@ public class TeamCityPluginServicesImpl implements CIPluginServices {
 
 	@Override
 	public CIPluginInfo getPluginInfo() {
-		return DTOFactory.getInstance().newDTO(CIPluginInfo.class)
+		return dtoFactory.newDTO(CIPluginInfo.class)
 				.setVersion(pluginVersion);
 	}
 
@@ -57,17 +61,25 @@ public class TeamCityPluginServicesImpl implements CIPluginServices {
 	@Override
 	public NGAConfiguration getNGAConfiguration() {
 		NGAConfig config = NGAPlugin.getInstance().getConfig();
-		return DTOFactory.getInstance().newDTO(NGAConfiguration.class)
+		return dtoFactory.newDTO(NGAConfiguration.class)
 				.setUrl(config.getLocation())
 				.setSharedSpace(config.getSharedSpace())
 				.setApiKey(config.getUsername())
 				.setSecret(config.getSecretPassword());
 	}
 
-	//  TODO: to be implemented
 	@Override
 	public CIProxyConfiguration getProxyConfiguration() {
-		return null;
+		CIProxyConfiguration result = null;
+		if (isProxyNeeded()) {
+			Map<String, String> propertiesMap = parseProperties(System.getenv("TEAMCITY_SERVER_OPTS"));
+			result = dtoFactory.newDTO(CIProxyConfiguration.class)
+					.setHost(propertiesMap.get("Dhttps.proxyHost"))
+					.setPort(Integer.parseInt(propertiesMap.get("Dhttps.proxyPort")))
+					.setUsername("")
+					.setPassword("");
+		}
+		return result;
 	}
 
 	@Override
@@ -101,5 +113,59 @@ public class TeamCityPluginServicesImpl implements CIPluginServices {
 	@Override
 	public BuildHistory getHistoryPipeline(String ciJobId, String originalBody) {
 		return DTOFactory.getInstance().newDTO(BuildHistory.class);
+	}
+
+
+	//	private static void configureProxy(String clientType, URL locationUrl, MqmConnectionConfig clientConfig, String username) {
+//		if (clientType.equals(ConfigurationService.CLIENT_TYPE)) {
+//			if (isProxyNeeded(locationUrl.getHost())) {
+//				clientConfig.setProxyHost(getProxyHost());
+//				clientConfig.setProxyPort(getProxyPort());
+//				final String proxyUsername = getUsername();
+//				if (!proxyUsername.isEmpty()) {
+//					clientConfig.setProxyCredentials(new UsernamePasswordProxyCredentials(username, getPassword()));
+//				}
+//			}
+//
+//		}
+//	}
+//
+//
+	private static boolean isProxyNeeded() {
+		Map<String, String> propertiesMap = parseProperties(System.getenv("TEAMCITY_SERVER_OPTS"));
+		return propertiesMap.get("Dhttps.proxyHost") != null;
+
+//		if (propertiesMap.get("Dhttps.proxyHost") == null) {
+//			return false;
+//		}
+//		host = propertiesMap.get("Dhttps.proxyHost");
+//		if (propertiesMap.get("Dhttps.proxyPort") != null) {
+//			port = Integer.parseInt(propertiesMap.get("Dhttps.proxyPort"));
+//		}
+//
+//		return true;
+/*
+                -Dproxyset=true
+                -Dhttp.proxyHost=proxy.domain.com
+                -Dhttp.proxyPort=8080
+                -Dhttp.nonProxyHosts=domain.com
+                -Dhttps.proxyHost=web-proxy.il.hpecorp.net
+                -Dhttps.proxyPort=8080
+                -Dhttps.nonProxyHosts=domain.com
+                */
+	}
+
+	private static Map<String, String> parseProperties(String internalProperties) {
+		Map<String, String> propertiesMap = new HashMap<String, String>();
+		if (internalProperties != null) {
+			String[] properties = internalProperties.split(" -");
+			for (String str : Arrays.asList(properties)) {
+				String[] split = str.split("=");
+				if (split.length == 2) {
+					propertiesMap.put(split[0], split[1]);
+				}
+			}
+		}
+		return propertiesMap;
 	}
 }
