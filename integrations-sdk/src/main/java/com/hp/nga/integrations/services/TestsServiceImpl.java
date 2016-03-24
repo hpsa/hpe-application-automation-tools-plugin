a00c047a4364ad54ac1c6f28ec67c74eb75ac815
Adi Lev <adi.lev@hp.com>
2016-03-24 15:58:49 +0200

    User Story #10774: JI: add support push  test result in TC - E2E  (level1)

--- integrations-sdk/src/main/java/com/hp/nga/integrations/services/TestsServiceImpl.java
+++ integrations-sdk/src/main/java/com/hp/nga/integrations/services/TestsServiceImpl.java
@@ -12,115 +12,124 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.*;
 
 /**
  * Created by gullery on 09/03/2016.
  */
 
 class TestsServiceImpl implements TestsService {
-	private static final Logger logger = LogManager.getLogger(TestsServiceImpl.class);
+    private static final Logger logger = LogManager.getLogger(TestsServiceImpl.class);
 
-	private int DATA_SEND_INTERVAL = 60000;
-	private static final DTOFactory dtoFactory = DTOFactory.getInstance();
-	private final SDKManager sdk;
-	private static BlockingQueue<BuildNode> blockingQueue = new LinkedBlockingDeque<BuildNode>(Integer.MAX_VALUE);
-	private final Object INIT_LOCKER = new Object();
-	private static final int IOEXCEPTION_CODE = -1;
-	private Thread worker;
-
-
-	TestsServiceImpl(SDKManager sdk) {
-		this.sdk = sdk;
-		activate();
-	}
-
-	public int pushTestsResult(TestResult testResult) {
-		NGARestClient restClient = sdk.getInternalService(NGARestService.class).obtainClient();
-		Map<String, String> headers = new HashMap<String, String>();
-		headers.put("content-type", "application/xml");
-		NGARequest request = dtoFactory.newDTO(NGARequest.class)
-				.setMethod(NGAHttpMethod.POST)
-				.setUrl(sdk.getCIPluginServices().getNGAConfiguration().getUrl() + "/api/shared_spaces/" +
-						sdk.getCIPluginServices().getNGAConfiguration().getSharedSpace() + "/workspaces/1002/test-results?skip-errors=false")
-				.setHeaders(headers)
-				.setBody(dtoFactory.dtoToXml(testResult));
-		try {
-			NGAResponse response = restClient.execute(request);
-			logger.info("tests result pushed with message " + response);
-			return response.getStatus();
-		} catch (IOException ioe) {
-			logger.error("failed to push tests result", ioe);
-			return IOEXCEPTION_CODE;
-		}
-	}
-
-	public void enqueuePushTestsResult(String ciJobRefId, String ciBuildRefId) {
-		blockingQueue.add(new BuildNode(ciJobRefId, ciBuildRefId));
-	}
-
-	private void activate() {
-		if (worker == null || !worker.isAlive()) {
-			synchronized (INIT_LOCKER) {
-				if (worker == null || !worker.isAlive()) {
-					worker = new Thread(new Runnable() {
-
-						public void run() {
-							while (true) {
-								try {
-									BuildNode buildNode = blockingQueue.take();
-									TestResult testResult = sdk.getCIPluginServices().getTestResults(buildNode.getCiJobRefId(), buildNode.getCiBuildRefId());
-									if(testResult != null){
-										int status = pushTestsResult(testResult);
-										if (status == HttpStatus.SC_SERVICE_UNAVAILABLE || status == IOEXCEPTION_CODE) {
-											blockingQueue.add(buildNode);
-											logger.info("tests result push failed because of service unavailable. retry pushing ");
-											Thread.sleep(DATA_SEND_INTERVAL);
-										}
-									}
-								} catch (Exception e) {
-									logger.error("TEST PUSH: failed to send test results", e);
-								}
-							}
-						}
-					});
-					worker.setDaemon(true);
-					worker.setName("TestPushWorker");
-					worker.start();
-				}
-			}
-		}
-	}
+    private int DATA_SEND_INTERVAL = 60000;
+    private int LIST_EMPTY_INTERVAL = 3000;
+    private static final DTOFactory dtoFactory = DTOFactory.getInstance();
+    private final SDKManager sdk;
+    private static List<BuildNode> buildList = Collections.synchronizedList(new LinkedList<BuildNode>());
+    private final Object INIT_LOCKER = new Object();
+    private Thread worker;
 
 
+    TestsServiceImpl(SDKManager sdk) {
+        this.sdk = sdk;
+        activate();
+    }
 
-	class BuildNode {
-		String ciJobRefId;
-		String ciBuildRefId;
+    public NGAResponse pushTestsResult(TestResult testResult) throws IOException{
+        NGARestClient restClient = sdk.getInternalService(NGARestService.class).obtainClient();
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("content-type", "application/xml");
+        NGARequest request = dtoFactory.newDTO(NGARequest.class)
+                .setMethod(NGAHttpMethod.POST)
+                .setUrl(sdk.getCIPluginServices().getNGAConfiguration().getUrl() + "/api/shared_spaces/" +
+                        sdk.getCIPluginServices().getNGAConfiguration().getSharedSpace() + "/workspaces/1002/test-results?skip-errors=false")
+                .setHeaders(headers)
+                .setBody(dtoFactory.dtoToXml(testResult));
+        NGAResponse response = restClient.execute(request);
+        logger.info("tests result pushed with message " + response);
+        return response;
+
+    }
+
+    public void enqueuePushTestsResult(String ciJobRefId, String ciBuildRefId) {
+        buildList.add(new BuildNode(ciJobRefId, ciBuildRefId));
+    }
+
+    private void activate() {
+        if (worker == null || !worker.isAlive()) {
+            synchronized (INIT_LOCKER) {
+                if (worker == null || !worker.isAlive()) {
+                    worker = new Thread(new Runnable() {
+
+                        public void run() {
+                            while (true) {
+                                if (!buildList.isEmpty()) {
+                                    try {
+                                        BuildNode buildNode = buildList.get(0);
+                                        TestResult testResult = sdk.getCIPluginServices().getTestResults(buildNode.getCiJobRefId(), buildNode.getCiBuildRefId());
+                                        if (testResult != null) {
+                                            NGAResponse response = pushTestsResult(testResult);
+                                            if (response.getStatus() == HttpStatus.SC_ACCEPTED) {
+                                                logger.info("Push test result was successful ");
+                                                buildList.remove(0);
+                                            } else if (response.getStatus() == HttpStatus.SC_SERVICE_UNAVAILABLE) {
+                                                logger.info("tests result push failed because of service unavailable. retry pushing ");
+                                                Thread.sleep(DATA_SEND_INTERVAL);
+                                            }
+                                        }
+                                    } catch (InterruptedException e) {
+                                        logger.error("Test Push Result - InterruptedException while sleeping", e);
+                                    } catch (IOException e) {
+                                        logger.error("Test Push IOException - retry pushing", e);
+                                        try {
+                                            Thread.sleep(DATA_SEND_INTERVAL);
+                                        } catch (InterruptedException e1) {
+                                            logger.error("Test Push Result - InterruptedException while sleeping after IOException ", e);
+                                        }
+                                    }
+                                } else {
+                                    try {
+                                        Thread.sleep(LIST_EMPTY_INTERVAL);
+                                    } catch (InterruptedException ie) {
+                                        logger.error("Test Push Result - InterruptedException while waiting for list to get builds", ie);
+                                    }
+                                }
+                            }
+                        }
+                    });
+                    worker.setDaemon(true);
+                    worker.setName("TestPushWorker");
+                    worker.start();
+                }
+            }
+        }
+    }
 
 
-		BuildNode(String ciJobRefId, String ciBuildRefId) {
-			this.ciJobRefId = ciJobRefId;
-			this.ciBuildRefId = ciBuildRefId;
-		}
+    class BuildNode {
+        String ciJobRefId;
+        String ciBuildRefId;
 
-		String getCiJobRefId() {
-			return ciJobRefId;
-		}
 
-		void setCiJobRefId(String ciJobRefId) {
-			this.ciJobRefId = ciJobRefId;
-		}
+        BuildNode(String ciJobRefId, String ciBuildRefId) {
+            this.ciJobRefId = ciJobRefId;
+            this.ciBuildRefId = ciBuildRefId;
+        }
 
-		String getCiBuildRefId() {
-			return ciBuildRefId;
-		}
+        String getCiJobRefId() {
+            return ciJobRefId;
+        }
 
-		void setCiBuildRefId(String ciBuildRefId) {
-			this.ciBuildRefId = ciBuildRefId;
-		}
-	}
+        void setCiJobRefId(String ciJobRefId) {
+            this.ciJobRefId = ciJobRefId;
+        }
+
+        String getCiBuildRefId() {
+            return ciBuildRefId;
+        }
+
+        void setCiBuildRefId(String ciBuildRefId) {
+            this.ciBuildRefId = ciBuildRefId;
+        }
+    }
 }
