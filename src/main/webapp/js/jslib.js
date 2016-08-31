ae1c3fbf46805023bf08601bcc070b491d9f6d92
YafimK <kazak@hpe.com>
2016-08-31 17:36:45 +0300

    New branch - due to removal of old node modules system.

---
+++ src/main/webapp/js/jslib.js
@@ -0,0 +1,89 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+// var Chart = require('chart.js');
+
+foo.getTotalHitsGraphData(function(t){
+    var r = t.responseObject();
+    var ctx = document.getElementById('mycanvas').getContext("2d");
+    new Chart(ctx).Line(r);
+});
+
+
+foo.getAvgHitsPerSecGraphData(function(t){
+    var data = t.responseObject();
+
+    var option = {};
+    var ctx = document.getElementById('AvgHitsPerSecGraphData').getContext("2d");
+    new Chart(ctx).Line(data);
+});
+
+foo.getTotalThroughputGraphData(function(t){
+    var data = t.responseObject();
+    var ctx = document.getElementById('TotalThroughput').getContext("2d");
+    new Chart(ctx).Line(data);
+
+});
+
+
+
+
+var Hello = React.createClass({
+    displayName: 'Hello',
+    render: function(){
+        return React.createElement('div', null, 'Hello ', this.props.name);
+    }
+});
+
+ReactDOM.render(
+    React.createElement(Hello, { name: 'World' }),
+    document.getElementById('container')
+);
+
+
+var ExampleApplication = React.createClass({
+    render: function () {
+        var elapsed = Math.round(this.props.elapsed / 100);
+        var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
+        var message = "React has been successfully running for " + seconds + "seconds.";
+
+        return React.DOM.p(null, message);
+    }
+});
+
+// Call React.createFactory instead of directly call ExampleApplication({...}) in React.render
+var ExampleApplicationFactory = React.createFactory(ExampleApplication);
+
+var start = new Date().getTime();
+setInterval(function () {
+    ReactDOM.render(
+        ExampleApplicationFactory({ elapsed: new Date().getTime() - start }),
+        document.getElementById('container')
+    );
+}, 50);
+
+
+
+//
+//
+// var cxz = {"2013-01-21":1,"2013-01-22":7};
+// var resultc = [];
+//
+//
+//
+// for(var i in cxz)
+//     resultc.push([i,cxz[i]]);
+//
+//
+//
+// var adf = JSON.parse(totalHitsGraphData);
+// document.getElementById('mse').innerHTML = JSON.stringify(resultc);
+// document.getElementById('msv').innerHTML = JSON.stringify(thgd);
+//
+// foo.getTotalHitsGraphData(function(t) {
+//     document.getElementById('msk').innerHTML = JSON.stringify(JSON.parse(t.responseObject()));
+// });
+//
+//
+// document.getElementById('msr').innerHTML = JSON.stringify(totalHitsGraphData);
+
