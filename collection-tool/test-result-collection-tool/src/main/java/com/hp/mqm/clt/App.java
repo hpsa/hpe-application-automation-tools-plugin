b9e57d4239f2e3e165af9d78f0f3495a2e6e0b75
Dana Shalev <dana.shalev@hpe.com>
2016-09-12 12:54:13 +0300

    tech: test-result-collection-tool - enable calling main API with Settings object

--- collection-tool/test-result-collection-tool/src/main/java/com/hp/mqm/clt/App.java
+++ collection-tool/test-result-collection-tool/src/main/java/com/hp/mqm/clt/App.java
@@ -5,6 +5,10 @@ public class App {
     public static void main(String[] args) {
         CliParser cliParser = new CliParser();
         Settings settings = cliParser.parse(args);
+        collectAndPushTestResults(settings);
+    }
+
+    public static void collectAndPushTestResults(Settings settings) {
         TestResultCollectionTool testResultCollectionTool = new TestResultCollectionTool(settings);
         testResultCollectionTool.collectAndPushTestResults();
     }
