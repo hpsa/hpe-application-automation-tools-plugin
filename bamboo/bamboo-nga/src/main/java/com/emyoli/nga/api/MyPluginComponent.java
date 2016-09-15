eddf8789a045ef7341facc787451c0980056f31d
lazara <ayellet.lazar@hpe.com>
2016-09-15 11:35:25 +0300

    part of US #94341: JI: [technical] CI/CD for Bamboo CI Server (+help from devops team)

--- bamboo/bamboo-nga/src/main/java/com/emyoli/nga/api/MyPluginComponent.java
+++
@@ -1,6 +0,0 @@
-package com.emyoli.nga.api;
-
-public interface MyPluginComponent
-{
-    String getName();
-}
---
+++ bamboo/nga-bamboo-plugin/src/main/java/com.hp.octane.plugins.bamboo/api/OctaneConfigurationKeys.java
@@ -0,0 +1,13 @@
+package com.hp.octane.plugins.bamboo.api;
+
+public interface OctaneConfigurationKeys {
+
+    String BAMBOO_INSTANCE_PREFIX = "bamboo-instance-";
+    String PLUGIN_PREFIX = "com.hp.octane.plugins.bamboo.";
+    String NGA_URL = PLUGIN_PREFIX + "ngaUrl";
+    String API_KEY = PLUGIN_PREFIX + "apiKey";
+    String API_SECRET = PLUGIN_PREFIX + "apiSecret";
+    String USER_TO_USE = PLUGIN_PREFIX + "userName";
+    String SHARED_SPACE_ID = PLUGIN_PREFIX + "sharedSpaceId";
+
+}
