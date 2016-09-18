1f5769329097732732d00a45bf8c69c2fd05c89a
Guller Yuri <yuri.guller@hp.com>
2016-09-18 11:22:59 +0300

    tech: cleaned some of bamboo's pom dependencies

--- bamboo/nga-bamboo-plugin/src/main/java/com.hp.octane.plugins.bamboo/listener/OctanePreJobAction.java
+++ bamboo/nga-bamboo-plugin/src/main/java/com.hp.octane.plugins.bamboo/listener/OctanePreJobAction.java
@@ -3,6 +3,7 @@ package com.hp.octane.plugins.bamboo.listener;
 import com.atlassian.bamboo.chains.StageExecution;
 import com.atlassian.bamboo.chains.plugins.PreJobAction;
 import com.atlassian.bamboo.plan.PlanResultKey;
+import com.atlassian.bamboo.v2.build.BuildContext;
 import com.hp.octane.integrations.OctaneSDK;
 import com.hp.octane.integrations.dto.causes.CIEventCause;
 import com.hp.octane.integrations.dto.events.CIEvent;
@@ -12,10 +13,8 @@ import java.util.Arrays;
 
 public class OctanePreJobAction extends BaseListener implements PreJobAction {
 
-    public void execute(StageExecution paramStageExecution,
-            com.atlassian.bamboo.v2.build.BuildContext paramBuildContext) {
+    public void execute(StageExecution paramStageExecution, BuildContext paramBuildContext) {
         PlanResultKey resultKey = paramBuildContext.getPlanResultKey();
-        // List<CIEventCause> causes = new ArrayList<>(1);
         CIEventCause cause = CONVERTER.getCauseWithDetails(
                 paramBuildContext.getParentBuildIdentifier().getBuildResultKey(),
                 paramBuildContext.getParentBuildContext().getPlanResultKey().getPlanKey().getKey(), "admin");
