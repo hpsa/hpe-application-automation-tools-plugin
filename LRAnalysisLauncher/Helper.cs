ee3782e5119f06f28a981b130601a99edeb62e66
YafimK <kazak@hpe.com>
2016-11-06 08:04:38 +0200

    Another fix for the StackOverflow issue with vusers

--- LRAnalysisLauncher/Helper.cs
+++ LRAnalysisLauncher/Helper.cs
@@ -47,7 +47,7 @@ namespace LRAnalysisLauncher
             {
                 //filterDimension.ClearValues();
                 //filterDimension.AddDiscreteValue(vUserType.Name);
-                vUserGraph.ApplyFilterAndGroupBy();
+                //vUserGraph.ApplyFilterAndGroupBy();
                 double vUserTypeMax = vUserType.GraphStatistics.Maximum;
                 if (!HasValue(vUserTypeMax)){
                     continue;
