67441e343dd5cc0d216217a6345d98f54bcfe23d
dulmang <dulman@hpe.com>
2016-11-24 18:40:26 +0200

    Adding signing result verification

--- scripts/Sign/signJarsInZip.sh
+++ scripts/Sign/signJarsInZip.sh
@@ -11,6 +11,12 @@ cp /opt/HPCSS/SignHP/SignHP_v3_1/login.conf .
 
 java -jar /opt/HPCSS/SignHP/SignHP_v3_1/SignHPUtility-1.4.02.jar -s "temp_folder/${jar_folder}/" -p AGN_JAR -i -v -u jarsigner -c 1 -o signed_folder -t 24 -f /opt/HPCSS/SignHP/SignHP_v3_1/sign.properties
 
+if [[ $? -ne 0 ]]
+then
+  echo ERROR signing zip file content - Check the logs;
+  exit 1;
+fi;
+
 cp signed_folder/*.jar temp_folder/${jar_folder}/
 cd temp_folder
 zip ../temp.zip -r *
