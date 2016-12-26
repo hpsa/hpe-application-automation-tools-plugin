004b2278cb6ac0d5c6c58fb42d103a7da4ab5bf5
Sharon Vardi <sharon.frank@hp.com>
2016-12-26 10:39:11 +0200

    user story #175065: Jenkins plugin is able to find the results file with no sever performance costs - Adjust to master-slave Jenkins structure
    (cherry picked from commit 53759a7)

--- jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/actions/cucumber/CucumberResultsService.java
+++ jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/actions/cucumber/CucumberResultsService.java
@@ -1,7 +1,10 @@
 package com.hp.octane.plugins.jenkins.actions.cucumber;
 
+import hudson.FilePath;
 import hudson.Util;
 import hudson.model.BuildListener;
+import hudson.remoting.VirtualChannel;
+import jenkins.MasterToSlaveFileCallable;
 import org.apache.tools.ant.DirectoryScanner;
 import org.apache.tools.ant.types.FileSet;
 import com.hp.octane.plugins.jenkins.Messages;
@@ -14,39 +17,82 @@ import java.nio.file.Files;
  */
 public class CucumberResultsService {
 
-  public static final String GHERKIN_NGA_RESULTS_XML = "OctaneCucumberResults.xml";
-  public static final String GHERKIN_NGA_RESULTS = "OctaneCucumberResults";
-  public static final String DEFAULT_GLOB = "**/" + GHERKIN_NGA_RESULTS_XML;
+    public static final String GHERKIN_NGA_RESULTS_XML = "OctaneGherkinResults.xml";
+    public static final String GHERKIN_NGA_RESULTS = "OctaneGherkinResults";
+    public static final String DEFAULT_GLOB = "**/" + GHERKIN_NGA_RESULTS_XML;
 
-  public static String getGherkinResultFileName(int index) {
-    return GHERKIN_NGA_RESULTS + index + ".xml";
-  }
+    private static BuildListener listener;
 
-  public static String[] getCucumberResultFiles(final File rootDir, String glob, final BuildListener listener) throws IOException, InterruptedException {
-      if (glob.isEmpty() || glob == null) {
-        glob = DEFAULT_GLOB;
-        log(listener, Messages.CucumberResultsActionEmptyConfiguration(), glob);
-      }
+    public static String getGherkinResultFileName(int index) {
+        return GHERKIN_NGA_RESULTS + index + ".xml";
+    }
 
-      FileSet fs = Util.createFileSet(rootDir, glob);
-      DirectoryScanner ds = fs.getDirectoryScanner();
-      String[] files = ds.getIncludedFiles();
-      return files;
-  }
+    public static String[] getCucumberResultFiles(final FilePath workspace, String glob) throws IOException, InterruptedException {
+        if (glob.isEmpty() || glob == null) {
+            glob = DEFAULT_GLOB;
+            log(Messages.CucumberResultsActionEmptyConfiguration(), glob);
+        }
 
-  public static void copyResultFile(File resultFile, File destinationFolder) throws IOException {
-    File existingReportFile;
-    int existingResultIndex = -1;
-    do {
-      existingReportFile = new File(destinationFolder, getGherkinResultFileName(++existingResultIndex));
-    } while (existingReportFile.exists());
+        log("Looking for files that match the pattern %s in root directory %s", glob, workspace.getName());
+        return workspace.act(new ResultFilesCallable(glob));
+    }
 
-    Files.copy(resultFile.toPath(), new File(destinationFolder, getGherkinResultFileName(existingResultIndex)).toPath());
-  }
+    public static void copyResultFile(File resultFile, File destinationFolder, final FilePath workspace) throws IOException, InterruptedException {
+        File existingReportFile;
+        int existingResultIndex = -1;
 
-  public static void log(final BuildListener listener, final String message, final String... stringFormatArgs) {
-      listener.getLogger().println(String.format("%s: %s",
-          Messages.CucumberReporterName(), String.format(message, stringFormatArgs)));
-  }
+        log("Copying %s to %s", resultFile.getPath(), destinationFolder.getPath());
+
+        do {
+            existingReportFile = new File(destinationFolder, getGherkinResultFileName(++existingResultIndex));
+        } while (existingReportFile.exists());
+        log("New file name on destination will be %s", existingReportFile.getPath());
+
+        byte[] content = workspace.act(new FileContentCallable(resultFile));
+        log("Got result file content");
+        File target = existingReportFile;
+        try (FileOutputStream os = new FileOutputStream(target)) {
+            os.write(content);
+        }
+        log("Result file copied to %s", target.getPath());
+    }
+
+    public static void log(final String message, final String... stringFormatArgs) {
+        if(listener != null) {
+            listener.getLogger().println(String.format("%s: %s",
+                Messages.CucumberReporterName(), String.format(message, stringFormatArgs)));
+        }
+    }
+
+    public static void setListener(BuildListener l) {
+        listener = l;
+    }
+
+    private static final class ResultFilesCallable extends MasterToSlaveFileCallable<String[]> {
+        private final String glob;
+
+        private ResultFilesCallable(String glob) {
+            this.glob = glob;
+        }
+
+        public String[] invoke(File rootDir, VirtualChannel channel) throws IOException {
+            FileSet fs = Util.createFileSet(rootDir, glob);
+            DirectoryScanner ds = fs.getDirectoryScanner();
+            String[] files = ds.getIncludedFiles();
+            return files;
+        }
+    }
+
+    private static final class FileContentCallable extends MasterToSlaveFileCallable<byte[]> {
+        private final File file;
+
+        private FileContentCallable(File file) {
+            this.file = file;
+        }
+
+        public byte[] invoke(File rootDir, VirtualChannel channel) throws IOException {
+            return Files.readAllBytes(file.toPath());
+        }
+    }
 
 }
--- jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/actions/cucumber/CucumberTestResultsAction.java
+++ jenkins/plugin/src/main/java/com/hp/octane/plugins/jenkins/actions/cucumber/CucumberTestResultsAction.java
@@ -14,35 +14,33 @@ import java.io.File;
 public class CucumberTestResultsAction implements Action {
     private final String glob;
     private final AbstractBuild build;
-    private final BuildListener listener;
 
     CucumberTestResultsAction(AbstractBuild build, String glob, BuildListener listener) {
         this.build = build;
         this.glob = glob;
-        this.listener = listener;
+        CucumberResultsService.setListener(listener);
     }
 
     public boolean copyResultsToBuildFolder() {
         try {
-            CucumberResultsService.log(listener, Messages.CucumberResultsActionCollecting());
-            File workspaceDir = new File(build.getWorkspace().toURI());
-            String[] files = CucumberResultsService.getCucumberResultFiles(workspaceDir, glob, listener);
+            CucumberResultsService.log(Messages.CucumberResultsActionCollecting());
+            String[] files = CucumberResultsService.getCucumberResultFiles(build.getWorkspace(), glob);
             boolean found = files.length > 0;
 
             for (String fileName : files) {
                 File resultFile = new File(build.getWorkspace().child(fileName).toURI());
-                CucumberResultsService.copyResultFile(resultFile, build.getRootDir());
+                CucumberResultsService.copyResultFile(resultFile, build.getRootDir(), build.getWorkspace());
             }
 
             if (!found && build.getResult() != Result.FAILURE) {
                 // most likely a configuration error in the job - e.g. false pattern to match the cucumber result files
-                CucumberResultsService.log(listener, Messages.CucumberResultsActionNotFound());
+                CucumberResultsService.log(Messages.CucumberResultsActionNotFound());
             }  // else , if results weren't found but build result is failure - most likely a build failed before us. don't report confusing error message.
 
             return found;
 
         } catch (Exception e) {
-            CucumberResultsService.log(listener, Messages.CucumberResultsActionError(), e.getMessage());
+            CucumberResultsService.log(Messages.CucumberResultsActionError(), e.toString());
             return false;
         }
     }
