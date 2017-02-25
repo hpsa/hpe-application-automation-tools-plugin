221d38be8698af10e5ca0aa02cf0a64b4d12e871
Michael Seldin <michael.seldin@hpe.com>
2017-02-25 02:06:10 +0200

    add ignored files.

---
+++ src/main/java/com/hp/application/automation/tools/octane/tests/build/BuildDescriptor.java
@@ -0,0 +1,54 @@
+/*
+ *     Copyright 2017 Hewlett-Packard Development Company, L.P.
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
+package com.hp.application.automation.tools.octane.tests.build;
+
+public final class BuildDescriptor {
+
+	private final String jobId;
+	private final String jobName;
+	private final String buildId;
+	private final String buildName;
+	private final String subType;
+
+	public BuildDescriptor(String jobId, String jobName, String buildId, String buildName, String subType) {
+		this.jobId = jobId;
+		this.jobName = jobName;
+		this.buildId = buildId;
+		this.buildName = buildName;
+		this.subType = subType;
+	}
+
+	public String getJobId() {
+		return jobId;
+	}
+
+	public String getJobName() {
+		return jobName;
+	}
+
+	public String getBuildId() {
+		return buildId;
+	}
+
+	public String getBuildName() {
+		return buildName;
+	}
+
+	public String getSubType() {
+		return subType;
+	}
+}
---
+++ src/main/java/com/hp/application/automation/tools/octane/tests/build/MatrixBuildExtension.java
@@ -0,0 +1,51 @@
+/*
+ *     Copyright 2017 Hewlett-Packard Development Company, L.P.
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
+package com.hp.application.automation.tools.octane.tests.build;
+
+import com.hp.octane.integrations.dto.parameters.CIParameter;
+import com.hp.application.automation.tools.octane.model.ModelFactory;
+import com.hp.application.automation.tools.octane.model.processors.parameters.ParameterProcessors;
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+
+import java.util.List;
+
+@Extension
+public class MatrixBuildExtension extends BuildHandlerExtension {
+
+	@Override
+	public boolean supports(AbstractBuild<?, ?> build) {
+		return "hudson.matrix.MatrixRun".equals(build.getClass().getName());
+	}
+
+	@Override
+	public BuildDescriptor getBuildType(AbstractBuild<?, ?> build) {
+		List<CIParameter> parameters = ParameterProcessors.getInstances(build);
+		String subBuildName = ModelFactory.generateSubBuildName(parameters);
+		return new BuildDescriptor(
+				build.getRootBuild().getProject().getName(),
+				build.getRootBuild().getProject().getName(),
+				String.valueOf(build.getNumber()),
+				String.valueOf(build.getNumber()),
+				subBuildName);
+	}
+
+	@Override
+	public String getProjectFullName(AbstractBuild<?, ?> build) {
+		return build.getRootBuild().getProject().getName() + "/" + build.getProject().getName();
+	}
+}
