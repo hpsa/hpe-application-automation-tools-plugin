23c55180596e4a815f3ae42e180ace50a09839ca
YafimK <yafim.kazak@gmail.com>
2017-03-29 09:03:48 +0300

    Fixed parsing of Lr script to JUnit Added New html report action

--- src/main/java/com/hp/application/automation/tools/run/RunLRScript.java
+++ src/main/java/com/hp/application/automation/tools/run/RunLRScript.java
@@ -1,5 +1,7 @@
 package com.hp.application.automation.tools.run;
 
+import com.hp.application.automation.tools.results.lrscriptresultparser.LrScriptHtmlReportAction;
+import com.hp.application.automation.tools.results.lrscriptresultparser.LrScriptResultsSanitizer;
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.FilePath;
@@ -9,34 +11,22 @@ import hudson.model.Computer;
 import hudson.model.Result;
 import hudson.model.Run;
 import hudson.model.TaskListener;
-import hudson.remoting.VirtualChannel;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
-import hudson.tasks.junit.JUnitParser;
 import hudson.tasks.junit.JUnitResultArchiver;
-import hudson.tasks.junit.TestResult;
 import hudson.util.ArgumentListBuilder;
-import jenkins.MasterToSlaveFileCallable;
-import jenkins.SlaveToMasterFileCallable;
 import jenkins.model.Jenkins;
-import jenkins.security.Roles;
 import jenkins.tasks.SimpleBuildStep;
+import jenkins.util.VirtualFile;
 import net.sf.json.JSONObject;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.input.BOMInputStream;
 import org.jenkinsci.Symbol;
-import org.jenkinsci.remoting.RoleChecker;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import javax.annotation.Nonnull;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
@@ -45,131 +35,199 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.net.URL;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CodingErrorAction;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 /**
  * Created by kazaky on 14/03/2017.
  */
-public class RunLRScript extends Builder implements SimpleBuildStep {
-    public static final String LINUX_MDRV_PATH = "/bin/mdrv";
-    public static final String WIN_MDRV_PATH = "\\bin\\mmdrv.exe";
 
-    private final String _scriptsPath;
+/**
+ * This step enables to run LoadRunner scripts directly and collecting their results by converting them to JUnit
+ */
+public class RunLRScript extends Builder implements SimpleBuildStep {
+    public static final String LR_SCRIPT_HTML_REPORT_CSS = "PResults.css";
+    private static final String LINUX_MDRV_PATH = "/bin/mdrv";
+    private static final String WIN_MDRV_PATH = "\\bin\\mmdrv.exe";
+    private static final String LR_SCRIPT_HTML_XSLT = "\\lib\\LR\\PDetails.xsl";
+    private static final String LR_SCRIPT_HTML_CSS = "\\lib\\LR\\LR_SCRIPT_REPORT.css";
+    private final String lrScriptPath;
+    private Jenkins jenkinsInstance;
     private PrintStream logger;
 
     @DataBoundConstructor
     public RunLRScript(String scriptsPath) {
-        this._scriptsPath = scriptsPath;
+        this.lrScriptPath = scriptsPath;
     }
 
 
     @Override
     public void perform(@Nonnull Run<?, ?> build, @Nonnull FilePath workspace, @Nonnull Launcher launcher,
                         @Nonnull TaskListener listener) throws InterruptedException, IOException {
-
-        logger = listener.getLogger();
-        ArgumentListBuilder args = new ArgumentListBuilder();
-        FilePath mdrv;
-        EnvVars env = null;
         try {
+            jenkinsInstance = Jenkins.getInstance();
+            logger = listener.getLogger();
+            ArgumentListBuilder args = new ArgumentListBuilder();
+            FilePath mdrv;
+            EnvVars env = null;
+
             env = build.getEnvironment(listener);
-        } catch (IOException | InterruptedException e) {
-            listener.error("Failed loading build environment " + e);
-        }
-
-        FilePath buildWorkDir = workspace.child(build.getId());
 
 
-        //base command line -usr "%1\%1.usr" -extra_ext NVReportExt -qt_result_dir "c:\%1_results"
-        //Do run the script on linux or windows?
-        mdrv = getMDRVPath(launcher, env);
-        args.add(mdrv);
+            //base command line mmdrv.exe -usr "%1\%1.usr" -extra_ext NVReportExt -qt_result_dir "c:\%1_results"
+            //Do run the script on linux or windows?
+            mdrv = getMDRVPath(launcher, env);
+            args.add(mdrv);
 
-        FilePath scriptPath = workspace.child(this._scriptsPath);
-        args.add("-usr");
-        args.add(scriptPath);
-        args.add("-extra_ext NVReportExt");
 
-        args.add("-qt_result_dir");
-        buildWorkDir.mkdirs();
-        buildWorkDir = buildWorkDir.absolutize();
-        String scriptName = FilenameUtils.getBaseName(this._scriptsPath);
-        if (Objects.equals(scriptName, "")) {
-            //TODO: if "" it's folder and we need to scan it
+            FilePath scriptPath = workspace.child(this.lrScriptPath);
+            args.add("-usr");
+            args.add(scriptPath);
+            args.add("-extra_ext NVReportExt");
 
-        }
-        FilePath scriptWorkDir = buildWorkDir.child(scriptName);
-        scriptWorkDir.mkdirs();
-        scriptWorkDir = scriptWorkDir.absolutize();
-        args.add(scriptWorkDir);
+            args.add("-qt_result_dir");
 
-        try {
+            FilePath buildWorkDir = workspace.child(build.getId());
+            buildWorkDir.mkdirs();
+            buildWorkDir = buildWorkDir.absolutize();
+
+            String scriptName = FilenameUtils.getBaseName(this.lrScriptPath);
+            if (Objects.equals(scriptName, "")) {
+                //TODO: if "" it's folder and we need to scan it
+
+            }
+            FilePath scriptWorkDir = buildWorkDir.child(scriptName);
+            scriptWorkDir.mkdirs();
+            scriptWorkDir = scriptWorkDir.absolutize();
+            args.add(scriptWorkDir);
+
             int returnCode = launcher.launch().cmds(args).stdout(logger).pwd(scriptWorkDir).join();
             if (returnCode != 0) {
                 build.setResult(Result.FAILURE);
                 return;
             }
 
+
+            final VirtualFile root = build.getArtifactManager().root();
+
+            File masterBuildWorkspace = new File(new File(root.toURI()), "LRReport");
+            if (!masterBuildWorkspace.exists()) {
+                if (!root.exists()) {
+                    (new File(root.toURI())).mkdirs();
+                }
+                masterBuildWorkspace.mkdirs();
+            }
+
+            //This part is on the master - we convert the results to JUnit and HTML report using specialized XSLT
+            final String resultXmlPath = buildWorkDir.child(scriptName).child("Results.xml").absolutize()
+                    .getRemote();
+            if (resultXmlPath == null) {
+                listener.fatalError(resultXmlPath + "not found in resources");
+                return;
+            }
+
+
+            final URL xsltPath = jenkinsInstance.pluginManager.uberClassLoader.getResource(LR_SCRIPT_HTML_XSLT);
+
+
+            FilePath outputHTML = buildWorkDir.child(scriptName);
+            outputHTML.mkdirs();
+            outputHTML = outputHTML.child("result.html");
+            FilePath xsltOnNode = workspace.child("resultsHtml.xslt");
+            xsltOnNode.copyFrom(xsltPath);
+            if (xsltOnNode.exists()) {
+                logger.println("Found XSLT on slave");
+            }
+
+            createHtmlReports(buildWorkDir, scriptName, outputHTML, xsltOnNode);
+
+            LrScriptResultsParser lrScriptResultsParser = new LrScriptResultsParser();
+
+            lrScriptResultsParser.parseScriptResult(scriptName, build, buildWorkDir, launcher, listener);
+            copyScriptsResultToMaster(build, listener, buildWorkDir, new FilePath(masterBuildWorkspace));
+//        Thread.sleep(5000); //TODO: try to remove this and see if we still work
+
+            parseJunitResult(build, launcher, listener, buildWorkDir, scriptName);
+            synchronized (build) {
+                LrScriptHtmlReportAction action = build.getAction(LrScriptHtmlReportAction.class);
+                if (action == null) {
+                    action = new LrScriptHtmlReportAction(build);
+                    action.mergeResult(build, scriptName);
+                    build.addAction(action);
+                } else {
+                    action.mergeResult(build, scriptName);
+                }
+            }
+            build.setResult(Result.SUCCESS);
+
         } catch (IllegalArgumentException e) {
             build.setResult(Result.FAILURE);
             logger.println(e);
+        } catch (IOException | InterruptedException e) {
+            listener.error("Failed loading build environment " + e);
+
+        } catch (XMLStreamException e) {
+            listener.error(e.getMessage(), e);
+            build.setResult(Result.FAILURE);
         }
+    }
 
-        FilePath masterBuildWorkspace = new FilePath(build.getRootDir());
-
-        //This part is on the master - we convert the results to JUnit and HTML report using specialized XSLT
-        final String resultXmlPath = buildWorkDir.child(scriptName).child("Results.xml").absolutize()
-                .getRemote();
-        if (resultXmlPath == null) {
-            listener.fatalError(resultXmlPath + "not found in resources");
-            return;
-        }
-
-
-        final String libLR = "\\lib\\LR\\PDetails.xsl";
-        final String xsltPath = Jenkins.getInstance().pluginManager.uberClassLoader.getResource(libLR).getPath();
-
-
-        FilePath outputHTML = buildWorkDir.child(scriptName);
-        outputHTML.mkdirs();
-        outputHTML = outputHTML.child("result.html");
-        FilePath xsltOnNode = workspace.child("resultsHtml.xslt");
-        xsltOnNode.copyFrom(Jenkins.getInstance().pluginManager.uberClassLoader.getResource(libLR));
-        if (xsltOnNode.exists()) {
-            logger.println("Found XSLT on slave");
-        }
-        createHtmlReports(buildWorkDir, scriptName, outputHTML, xsltOnNode);
-        LrScriptResultsParser lrScriptResultsParser = new LrScriptResultsParser();
-        lrScriptResultsParser.parseScriptResult(scriptName, build, buildWorkDir, launcher, listener);
-        copyScriptsResultToMaster(build, listener, buildWorkDir, masterBuildWorkspace);
-        Thread.sleep(4000);
-
+    private void parseJunitResult(@Nonnull Run<?, ?> build, @Nonnull Launcher launcher, @Nonnull TaskListener listener,
+                                  FilePath buildWorkDir, String scriptName) throws InterruptedException, IOException {
         JUnitResultArchiver jUnitResultArchiver = new JUnitResultArchiver("JunitResult.xml");
         jUnitResultArchiver.setKeepLongStdio(true);
         jUnitResultArchiver.setAllowEmptyResults(true);
         jUnitResultArchiver.perform(build, buildWorkDir.child(scriptName), launcher, listener);
-
-
-        build.setResult(Result.SUCCESS);
     }
 
     private void createHtmlReports(FilePath buildWorkDir, String scriptName, FilePath outputHTML, FilePath xsltOnNode)
-            throws IOException, InterruptedException {
-        //TODO: check arguments
+            throws IOException, InterruptedException, XMLStreamException {
+        if (!buildWorkDir.exists()) {
+            throw new IllegalArgumentException("Build worker doesn't exist");
+        }
+        if ("".equals(scriptName)) {
+            throw new IllegalArgumentException("Script name is empty");
+        }
+        if (!xsltOnNode.exists()) {
+            throw new IllegalArgumentException("LR Html report doesn't exist on the node");
+        }
         try {
             TransformerFactory factory = TransformerFactory.newInstance();
             StreamSource xslStream = new StreamSource(xsltOnNode.read());
             Transformer transformer = factory.newTransformer(xslStream);
-            StreamSource in = new StreamSource(buildWorkDir.child(scriptName).child("Results.xml").read());
+
+            CharsetDecoder decoder = StandardCharsets.UTF_8.newDecoder();
+            decoder.onMalformedInput(CodingErrorAction.REPLACE).replacement();
+
+            final InputStreamReader inputStreamReader = new InputStreamReader(new BOMInputStream(buildWorkDir
+                    .child(scriptName).child("Results.xml").read()), decoder);
+
+            StreamSource in = new StreamSource(new LrScriptResultsSanitizer(inputStreamReader));
             StreamResult out = new StreamResult(outputHTML.write());
             transformer.transform(in, out);
+            final URL lrHtmlCSSPath = jenkinsInstance.pluginManager.uberClassLoader.getResource(LR_SCRIPT_HTML_CSS);
+            if (lrHtmlCSSPath == null) {
+                throw new LrScriptParserException(
+                        "For some reason the jenkins instance is null - is it an improper set tests?");
+            }
+
+            FilePath lrScriptHtmlReportCss = buildWorkDir.child(scriptName).child(LR_SCRIPT_HTML_REPORT_CSS);
+            lrScriptHtmlReportCss.copyFrom(lrHtmlCSSPath);
+
             logger.println("The generated HTML file is:" + outputHTML);
         } catch (TransformerConfigurationException e) {
             logger.println("TransformerConfigurationException");
             logger.println(e);
         } catch (TransformerException e) {
             logger.println("TransformerException");
+            logger.println(e);
+        } catch (LrScriptParserException e) {
+            logger.println("General exception");
             logger.println(e);
         }
     }
@@ -183,12 +241,12 @@ public class RunLRScript extends Builder implements SimpleBuildStep {
         buildWorkDir.copyRecursiveTo(masterBuildWorkspace);
     }
 
-    private FilePath getMDRVPath(@Nonnull Launcher launcher, EnvVars env) throws IllegalArgumentException {
+    private FilePath getMDRVPath(@Nonnull Launcher launcher, EnvVars env) throws LrScriptParserException {
         FilePath mdrv;
         if (launcher.isUnix()) {
             String lrPath = env.get("M_LROOT", "");
-            if (Objects.equals(lrPath, "")) {
-                throw new IllegalArgumentException(
+            if ("".equals(lrPath)) {
+                throw new LrScriptParserException(
                         "Please make sure environment variables are set correctly on the running node - " +
                                 "LR_PATH for windows and M_LROOT for linux");
             }
@@ -196,8 +254,8 @@ public class RunLRScript extends Builder implements SimpleBuildStep {
             mdrv = new FilePath(launcher.getChannel(), lrPath);
         } else {
             String lrPath = env.get("LR_PATH", "");
-            if (Objects.equals(lrPath, "")) {
-                throw new IllegalArgumentException("Please make sure enviroment varibals are set correctly on the " +
+            if ("".equals(lrPath)) {
+                throw new LrScriptParserException("P1lease make sure environment variables are set correctly on the " +
                         "running node - " +
                         "LR_PATH for windows and M_LROOT for linux");
             }
@@ -209,13 +267,12 @@ public class RunLRScript extends Builder implements SimpleBuildStep {
 
 
     public String getScriptsPath() {
-        return _scriptsPath;
+        return lrScriptPath;
     }
 
     @Symbol("RunLoadRunnerScript")
     @Extension
     public static class Descriptor extends BuildStepDescriptor<Builder> {
-        private String global;
 
         public Descriptor() {
             load();
@@ -227,8 +284,9 @@ public class RunLRScript extends Builder implements SimpleBuildStep {
         }
 
         @Override
+
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            global = formData.getString("scriptsPath");
+            formData.getString("scriptsPath");
             save();
             return super.configure(req, formData);
         }
@@ -240,4 +298,12 @@ public class RunLRScript extends Builder implements SimpleBuildStep {
 
     }
 
+
+    public static final class LrScriptParserException extends IllegalArgumentException {
+
+        public LrScriptParserException(String s) {
+            super(s);
+        }
+
+    }
 }
