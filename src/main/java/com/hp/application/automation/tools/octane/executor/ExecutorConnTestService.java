90cadabbedd9b051af303e1c970be2c03e0fcaee
Radislav Berkovich <radislav.berkovich@hpe.com>
2017-04-25 09:50:48 +0300

    fix style

--- src/main/java/com/hp/application/automation/tools/octane/executor/ExecutorConnTestService.java
+++ src/main/java/com/hp/application/automation/tools/octane/executor/ExecutorConnTestService.java
@@ -9,9 +9,7 @@ import hudson.EnvVars;
 import hudson.model.TaskListener;
 import hudson.plugins.git.GitException;
 import hudson.plugins.git.GitTool;
-import hudson.plugins.git.Messages;
 import hudson.plugins.git.UserRemoteConfig;
-import hudson.util.FormValidation;
 import org.jenkinsci.plugins.gitclient.Git;
 import org.jenkinsci.plugins.gitclient.GitClient;
 
@@ -22,35 +20,35 @@ import java.io.IOException;
  */
 public class ExecutorConnTestService {
 
-	public static boolean checkRepositoryConnectivity(TestConnectivityInfo testConnectivityInfo) {
-		if(testConnectivityInfo.getScmRepository()!=null && testConnectivityInfo.getScmRepository().getUrl() != null && !testConnectivityInfo.getScmRepository().getUrl().isEmpty()){
-			if(testConnectivityInfo.getScmRepository().getType() == SCMType.GIT){
-				BaseStandardCredentials c = null;
-				if(testConnectivityInfo.getUsername() != null && !testConnectivityInfo.getUsername().isEmpty()
-					&& testConnectivityInfo.getPassword() != null ){
-					c = new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,null,null,testConnectivityInfo.getUsername(),testConnectivityInfo.getPassword());
-				}
-				UserRemoteConfig.DescriptorImpl userRemoteConfig = new UserRemoteConfig.DescriptorImpl();
-				try {
+    public static boolean checkRepositoryConnectivity(TestConnectivityInfo testConnectivityInfo) {
+        if (testConnectivityInfo.getScmRepository() != null && testConnectivityInfo.getScmRepository().getUrl() != null && !testConnectivityInfo.getScmRepository().getUrl().isEmpty()) {
+            if (testConnectivityInfo.getScmRepository().getType() == SCMType.GIT) {
+                BaseStandardCredentials c = null;
+                if (testConnectivityInfo.getUsername() != null && !testConnectivityInfo.getUsername().isEmpty()
+                        && testConnectivityInfo.getPassword() != null) {
+                    c = new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL, null, null, testConnectivityInfo.getUsername(), testConnectivityInfo.getPassword());
+                }
+                UserRemoteConfig.DescriptorImpl userRemoteConfig = new UserRemoteConfig.DescriptorImpl();
+                try {
 //						FormValidation res = userRemoteConfig.doCheckUrl(null,c != null ? c.getId() : "",testConnectivityInfo.getScmRepository().getUrl());
 
-					EnvVars environment = new EnvVars(System.getenv());
-					GitClient git = Git.with(TaskListener.NULL, environment).using(GitTool.getDefaultInstallation().getGitExe()).getClient();
-					git.addDefaultCredentials(c);
+                    EnvVars environment = new EnvVars(System.getenv());
+                    GitClient git = Git.with(TaskListener.NULL, environment).using(GitTool.getDefaultInstallation().getGitExe()).getClient();
+                    git.addDefaultCredentials(c);
 
-					try {
-						git.getHeadRev(testConnectivityInfo.getScmRepository().getUrl(), "HEAD");
-					} catch (GitException e) {
-						return false;
-					}
-					return true;
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (InterruptedException e) {
-						e.printStackTrace();
-				}
-			}
-		}
-		return false;
-	}
+                    try {
+                        git.getHeadRev(testConnectivityInfo.getScmRepository().getUrl(), "HEAD");
+                    } catch (GitException e) {
+                        return false;
+                    }
+                    return true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
 }
