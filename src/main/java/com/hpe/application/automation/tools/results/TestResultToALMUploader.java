5d2613566814f1fe0dfce5c11c9bf532e4cd52d1
Roy-Lu <li.lu2@hpe.com>
2017-07-10 13:34:42 +0800

    Add variable resolver to fields and enable credential for Upload step (#218)
    * Support executing SSE on ALM SaaS
    
    * Remove the pattern check.
    
    * Support executing SSE on ALM SaaS
    1. Fixed some check style issues.
    2. Unify the rest authentication process here from separated part, ALMRestTool and RunManager.
    
    * Fixed some lint issues.
    
    * Fixed lint issues.
    
    * Fixed the alm server setting would be missing after rebranding.
    
    * Fixed the compile issue.
    
    * 1. Change username/password fields to credential in upload step.
    2. Add variable resolver to test folder, test set folder, Jenkins URL fields.
    3. Fix the alm server selection when initializing SSEbuilder page.
    
    * Make Jacky's old code better. Avoid throw NPE.

--- src/main/java/com/hpe/application/automation/tools/results/TestResultToALMUploader.java
+++ src/main/java/com/hpe/application/automation/tools/results/TestResultToALMUploader.java
@@ -5,6 +5,12 @@
 
 package com.hpe.application.automation.tools.results;
 
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardUsernameListBoxModel;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.common.UsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
+import com.cloudbees.plugins.credentials.matchers.IdMatcher;
 import com.hpe.application.automation.tools.model.AlmServerSettingsModel;
 import com.hpe.application.automation.tools.model.EnumDescription;
 import com.hpe.application.automation.tools.results.service.AlmRestInfo;
@@ -14,6 +20,7 @@ import com.hpe.application.automation.tools.results.service.IExternalEntityUploa
 import com.hpe.application.automation.tools.settings.AlmServerSettingsBuilder;
 import hudson.Extension;
 import hudson.Launcher;
+import hudson.Util;
 import hudson.matrix.MatrixAggregatable;
 import hudson.matrix.MatrixAggregator;
 import hudson.matrix.MatrixBuild;
@@ -22,7 +29,12 @@ import hudson.model.BuildListener;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Hudson;
+import hudson.model.Item;
+import hudson.model.Queue;
 import hudson.model.Result;
+import hudson.model.Run;
+import hudson.model.queue.Tasks;
+import hudson.security.ACL;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Publisher;
@@ -33,11 +45,15 @@ import hudson.util.FormValidation;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.Serializable;
 import java.util.List;
 
+import hudson.util.ListBoxModel;
+import hudson.util.VariableResolver;
 import org.apache.commons.lang.StringUtils;
 import org.apache.tools.ant.DirectoryScanner;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
@@ -52,13 +68,120 @@ import com.hpe.application.automation.tools.results.service.DefaultExternalEntit
 public class TestResultToALMUploader extends Recorder implements Serializable, MatrixAggregatable {
     
     private static final long serialVersionUID = 1L;
-    private final UploadTestResultToAlmModel uploadTestResultToAlmModel;
-    
+    private UploadTestResultToAlmModel uploadTestResultToAlmModel;
+    private String almServerName;
+    private String credentialsId;
+    private String almDomain;
+    private String almProject;
+    private String testingFramework;
+    private String testingTool;
+    private String almTestFolder;
+    private String almTestSetFolder;
+    private String almTimeout;
+    private String testingResultFile;
+    private String jenkinsServerUrl;
+
+    // These getters setters work for reading config.xml.
+    public UploadTestResultToAlmModel getUploadTestResultToAlmModel() {
+        return uploadTestResultToAlmModel;
+    }
+
+    public void setUploadTestResultToAlmModel(UploadTestResultToAlmModel uploadTestResultToAlmModel) {
+        this.uploadTestResultToAlmModel = uploadTestResultToAlmModel;
+    }
+
+    public String getAlmServerName() {
+        return almServerName;
+    }
+
+    public void setAlmServerName(String almServerName) {
+        this.almServerName = almServerName;
+    }
+
+    public String getCredentialsId() {
+        return credentialsId;
+    }
+
+    public void setCredentialsId(String credentialsId) {
+        this.credentialsId = credentialsId;
+    }
+
+    public String getAlmDomain() {
+        return almDomain;
+    }
+
+    public void setAlmDomain(String almDomain) {
+        this.almDomain = almDomain;
+    }
+
+    public String getAlmProject() {
+        return almProject;
+    }
+
+    public void setAlmProject(String almProject) {
+        this.almProject = almProject;
+    }
+
+    public String getTestingFramework() {
+        return testingFramework;
+    }
+
+    public void setTestingFramework(String testingFramework) {
+        this.testingFramework = testingFramework;
+    }
+
+    public String getTestingTool() {
+        return testingTool;
+    }
+
+    public void setTestingTool(String testingTool) {
+        this.testingTool = testingTool;
+    }
+
+    public String getAlmTestFolder() {
+        return almTestFolder;
+    }
+
+    public void setAlmTestFolder(String almTestFolder) {
+        this.almTestFolder = almTestFolder;
+    }
+
+    public String getAlmTestSetFolder() {
+        return almTestSetFolder;
+    }
+
+    public void setAlmTestSetFolder(String almTestSetFolder) {
+        this.almTestSetFolder = almTestSetFolder;
+    }
+
+    public String getAlmTimeout() {
+        return almTimeout;
+    }
+
+    public void setAlmTimeout(String almTimeout) {
+        this.almTimeout = almTimeout;
+    }
+
+    public String getTestingResultFile() {
+        return testingResultFile;
+    }
+
+    public void setTestingResultFile(String testingResultFile) {
+        this.testingResultFile = testingResultFile;
+    }
+
+    public String getJenkinsServerUrl() {
+        return jenkinsServerUrl;
+    }
+
+    public void setJenkinsServerUrl(String jenkinsServerUrl) {
+        this.jenkinsServerUrl = jenkinsServerUrl;
+    }
+
     @DataBoundConstructor
     public TestResultToALMUploader(
             String almServerName,
-            String almUserName,
-            String almPassword,
+            String credentialsId,
             String almDomain,
             String almProject,
             String testingFramework,
@@ -68,61 +191,56 @@ public class TestResultToALMUploader extends Recorder implements Serializable, M
             String almTimeout,
             String testingResultFile,
             String jenkinsServerUrl) {
-        
-    	uploadTestResultToAlmModel = new UploadTestResultToAlmModel( almServerName,  almUserName,
-    			 almPassword,  almDomain,  almProject, testingFramework, testingTool,
-    			 almTestFolder,  almTestSetFolder,  almTimeout, testingResultFile, jenkinsServerUrl);
+
+        this.almServerName = almServerName;
+        this.credentialsId = credentialsId;
+        this.almDomain = almDomain;
+        this.almProject = almProject;
+        this.testingFramework = testingFramework;
+        this.testingTool = testingTool;
+        this.almTestFolder = almTestFolder;
+        this.almTestSetFolder = almTestSetFolder;
+        this.almTimeout = almTimeout;
+        this.testingResultFile = testingResultFile;
+        this.jenkinsServerUrl = jenkinsServerUrl;
     }
-    
-    @Override
-    public DescriptorImpl getDescriptor() {
-        
-        return (DescriptorImpl) super.getDescriptor();
-    }
-    
-    public AlmServerSettingsModel getAlmServerSettingsModel() {
-        for (AlmServerSettingsModel almServer : getDescriptor().getAlmServers()) {
-            if (this.uploadTestResultToAlmModel != null
-                && uploadTestResultToAlmModel.getAlmServerName().equals(almServer.getAlmServerName())) {
-                return almServer;
-            }
-        }
-        return null;
-    }
-    
-    public UploadTestResultToAlmModel getUploadTestResultToAlmModel() {
-        return uploadTestResultToAlmModel;
-    }
-    
-    private String getAlmServerUrl(String almServerName) {
-    	AlmServerSettingsModel[] almServers = Hudson.getInstance().getDescriptorByType(
-                AlmServerSettingsBuilder.DescriptorImpl.class).getInstallations();
-    	if(almServers != null && almServers.length >0) {
-    		for(AlmServerSettingsModel almServerModel: almServers) {
-    			if(almServerName.equalsIgnoreCase(almServerModel.getAlmServerName())) {
-    				return almServerModel.getAlmServerUrl();
-    			}
-    		}
-    	}
-    	
-    	return "";
-    }
-    
+
     @Override
     public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
             throws InterruptedException, IOException {
-        
     	ExternalEntityUploadLogger logger = new ExternalEntityUploadLogger(listener.getLogger());
 
-    	logger.log("INFO: 'Upload test result to ALM' Post Build Step is being invoked.");
-        if(uploadTestResultToAlmModel == null){
-        	logger.log("ERROR: No configuration for 'Upload test reslt to ALM' step");
+    	// Credentials id maybe can't be blank
+        if (StringUtils.isBlank(credentialsId)) {
+            logger.log("INFO: credentials is not configured.");
+            build.setResult(Result.UNSTABLE);
+            return true;
         }
-        
-        
+        UsernamePasswordCredentials credentials = getCredentialsById(credentialsId, build, logger);
+
+
+    	logger.log(String.format("INFO: 'Upload test result to ALM' Post Build Step is being invoked by %s.",
+                credentials.getUsername()));
+
+        uploadTestResultToAlmModel = new UploadTestResultToAlmModel(
+                almServerName,
+                credentials.getUsername(),
+                credentials.getPassword().getPlainText(),
+                almDomain,
+                almProject,
+                testingFramework,
+                testingTool,
+                almTestFolder,
+                almTestSetFolder,
+                almTimeout,
+                testingResultFile,
+                jenkinsServerUrl);
+
+        VariableResolver<String> varResolver = new VariableResolver.ByMap<String>(build.getEnvironment(listener));
+
         String serverUrl = getAlmServerUrl(uploadTestResultToAlmModel.getAlmServerName());
         String runUrl = "";
-        String tempUrl = uploadTestResultToAlmModel.getJenkinsServerUrl();
+        String tempUrl = Util.replaceMacro(uploadTestResultToAlmModel.getJenkinsServerUrl(), varResolver);
         if(tempUrl != null && tempUrl.length() >0 ) {
         	if(tempUrl.charAt(tempUrl.length() -1) != '/') {
         		runUrl= tempUrl + "/" + build.getUrl();
@@ -130,15 +248,15 @@ public class TestResultToALMUploader extends Recorder implements Serializable, M
         		runUrl = tempUrl + build.getUrl();
         	}
         }
-        
+
         File root = build.getRootDir();
         DirectoryScanner ds = new DirectoryScanner();
         ds.setBasedir(root);
         ds.setIncludes( new String[] {uploadTestResultToAlmModel.getTestingResultFile()});
-        
         ds.scan();
         if (ds.getIncludedFilesCount() == 0) {
         	logger.log("INFO: No Test Report found.");
+            build.setResult(Result.UNSTABLE);
         } else {
         	logger.log("INFO: "+ ds.getIncludedFilesCount() +" test result file found.");
         	String[] files = ds.getIncludedFiles();
@@ -150,38 +268,79 @@ public class TestResultToALMUploader extends Recorder implements Serializable, M
     					uploadTestResultToAlmModel.getAlmProject(),
     					uploadTestResultToAlmModel.getAlmUserName(),
     					uploadTestResultToAlmModel.getAlmPassword(),
-    					uploadTestResultToAlmModel.getAlmTestSetFolder()
-    					);
+                        Util.replaceMacro(uploadTestResultToAlmModel.getAlmTestSetFolder(), varResolver)
+                );
     			AlmRestTool u = new AlmRestTool(loginInfo, logger);
     			logger.log("INFO: Start to upload "+fullpath);
     			IExternalEntityUploadService service = new DefaultExternalEntityUploadServiceImpl(u, logger );
     			try {
-	    			service.UploadExternalTestSet(loginInfo, 
-	    											fullpath, 
-	    											uploadTestResultToAlmModel.getAlmTestSetFolder(), 
-	    											uploadTestResultToAlmModel.getAlmTestFolder(), 
-	    											uploadTestResultToAlmModel.getTestingFramework(), 
-	    											uploadTestResultToAlmModel.getTestingTool(), 
-	    											String.valueOf(build.getNumber()),
-	    											build.getParent().getDisplayName(),
-	    											runUrl
-	    											);
+	    			service.UploadExternalTestSet(loginInfo,
+	    					fullpath,
+                            Util.replaceMacro(uploadTestResultToAlmModel.getAlmTestSetFolder(), varResolver),
+                            Util.replaceMacro(uploadTestResultToAlmModel.getAlmTestFolder(), varResolver),
+                            uploadTestResultToAlmModel.getTestingFramework(),
+                            uploadTestResultToAlmModel.getTestingTool(),
+                            String.valueOf(build.getNumber()),
+                            build.getParent().getDisplayName(),
+                            runUrl
+                    );
 	    			logger.log("INFO: Uploaded "+fullpath + ".");
     			} catch (Exception e) {
     				logger.log("WARN: there's exception while uploading "+fullpath + ".");
     				build.setResult(Result.UNSTABLE);
     			}
-
         	}
         }
-
         logger.log("INFO: 'Upload test result to ALM' Completed.");
-        
         return true;
     }
-    
 
+    /**
+     * Get user name password credentials by id.
+     */
+    private UsernamePasswordCredentials getCredentialsById(String credentialsId, Run<?, ?> run, ExternalEntityUploadLogger logger) {
+        UsernamePasswordCredentials credentials = CredentialsProvider.findCredentialById(credentialsId,
+                StandardUsernamePasswordCredentials.class,
+                run,
+                URIRequirementBuilder.create().build());
 
+        if (credentials == null) {
+            logger.log("Can not find credentials with the credentialsId:" + credentialsId);
+        }
+        return credentials;
+    }
+
+    private String getAlmServerUrl(String almServerName) {
+        AlmServerSettingsModel[] almServers = Hudson.getInstance().getDescriptorByType(
+                AlmServerSettingsBuilder.DescriptorImpl.class).getInstallations();
+        if(almServers != null && almServers.length >0) {
+            for(AlmServerSettingsModel almServerModel: almServers) {
+                if(almServerName.equalsIgnoreCase(almServerModel.getAlmServerName())) {
+                    return almServerModel.getAlmServerUrl();
+                }
+            }
+        }
+        return "";
+    }
+
+    /**
+     * This works for jelly
+     * <f:option selected="${almServer.almServerName==instance.almServerSettingsModel.almServerName}" value="${almServer.almServerName}">
+     * @return
+     */
+    public AlmServerSettingsModel getAlmServerSettingsModel() {
+        for (AlmServerSettingsModel almServer : getDescriptor().getAlmServers()) {
+            if (this.almServerName.equals(almServer.getAlmServerName())) {
+                return almServer;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl) super.getDescriptor();
+    }
     
     @Override
     public Action getProjectAction(AbstractProject<?, ?> project) {
@@ -284,6 +443,41 @@ public class TestResultToALMUploader extends Recorder implements Serializable, M
         
         public List<EnumDescription> getTestingFrameworks() {
             return UploadTestResultToAlmModel.testingFrameworks;
+        }
+
+        /**
+         * To fill in the credentials drop down list which's field is 'credentialsId'.
+         * This method's name works with tag <c:select/>.
+         */
+        public ListBoxModel doFillCredentialsIdItems(@AncestorInPath Item project,
+                                                     @QueryParameter String credentialsId) {
+            if (project == null || !project.hasPermission(Item.CONFIGURE)) {
+                return new StandardUsernameListBoxModel().includeCurrentValue(credentialsId);
+            }
+            return new StandardUsernameListBoxModel()
+                    .includeEmptyValue()
+                    .includeAs(
+                            project instanceof Queue.Task ? Tasks.getAuthenticationOf((Queue.Task) project) : ACL.SYSTEM,
+                            project,
+                            StandardUsernamePasswordCredentials.class,
+                            URIRequirementBuilder.create().build())
+                    .includeCurrentValue(credentialsId);
+        }
+
+        public FormValidation doCheckCredentialsId(@AncestorInPath Item project, @QueryParameter String value) {
+            for (ListBoxModel.Option o : CredentialsProvider.listCredentials(
+                    StandardUsernamePasswordCredentials.class,
+                    project,
+                    project instanceof Queue.Task ? Tasks.getAuthenticationOf((Queue.Task) project) : ACL.SYSTEM,
+                    URIRequirementBuilder.create().build(),
+                    new IdMatcher(value))) {
+
+                if (StringUtils.equals(value, o.value)) {
+                    return FormValidation.ok();
+                }
+            }
+            // no credentials available, can't check
+            return FormValidation.warning("Cannot find any credentials with id " + value);
         }
         
     }
