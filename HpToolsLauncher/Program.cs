a34679755a9bd5a6c82bba1e7dc1411fcdc72df1
Daniel Gront <grount1@gmail.com>
2018-08-13 10:21:31 +0300

    Remove unnecessary using methods

--- HpToolsLauncher/Program.cs
+++ HpToolsLauncher/Program.cs
@@ -22,9 +22,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading;
 using HpToolsLauncher.Properties;
 
 namespace HpToolsLauncher
