2634b8e76b27590b21cee113bd6d3507853ea82a
vaingart <vaingart@hpe.com>
2018-09-13 13:46:54 +0300

    tech: fix codacy issues

--- src/main/java/com/hpe/application/automation/tools/model/SonarHelper.java
+++ src/main/java/com/hpe/application/automation/tools/model/SonarHelper.java
@@ -17,9 +17,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Optional;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * the adapter enables usage of sonar classes and interfaces without adding a full dependency on
