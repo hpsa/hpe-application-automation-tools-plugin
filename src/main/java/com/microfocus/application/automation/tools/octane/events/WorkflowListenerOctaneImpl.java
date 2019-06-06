97be3eb6f5219cfebbebf97bbdff0b40e66f48b6
Radislav Berkovich <radislav.berkovich@microfocus.com>
2019-06-06 22:44:30 +0300

    tech : skip event handling if no client is defined in SDK

--- src/main/java/com/microfocus/application/automation/tools/octane/events/WorkflowListenerOctaneImpl.java
+++ src/main/java/com/microfocus/application/automation/tools/octane/events/WorkflowListenerOctaneImpl.java
@@ -21,6 +21,7 @@
 package com.microfocus.application.automation.tools.octane.events;
 
 import com.google.inject.Inject;
+import com.hp.octane.integrations.OctaneSDK;
 import com.hp.octane.integrations.dto.DTOFactory;
 import com.hp.octane.integrations.dto.events.CIEvent;
 import com.hp.octane.integrations.dto.events.CIEventType;
@@ -72,6 +73,9 @@ public class WorkflowListenerOctaneImpl implements GraphListener {
 
 	@Override
 	public void onNewHead(FlowNode flowNode) {
+		if(!OctaneSDK.hasClients()){
+			return;
+		}
 		try {
 			if (BuildHandlerUtils.isWorkflowStartNode(flowNode)) {
 				sendPipelineStartedEvent(flowNode);
