413381e52ef1f7a7c862c03d35d3993c7b79ef30
Radislav Berkovich <radislav.berkovich@microfocus.com>
2020-02-16 13:17:48 +0200

    tech : add log of loading time

--- src/main/java/com/microfocus/application/automation/tools/octane/events/GlobalEventsListenerOctaneImpl.java
+++ src/main/java/com/microfocus/application/automation/tools/octane/events/GlobalEventsListenerOctaneImpl.java
@@ -61,7 +61,9 @@ public class GlobalEventsListenerOctaneImpl extends ItemListener {
 		logger.info("Jenkins version " + Jenkins.getVersion());
 		logger.info("Plugin version " + ConfigurationService.getPluginVersion());
 
+		long startTime = System.currentTimeMillis();
 		OctaneServerSettingsBuilder.getOctaneSettingsManager().initOctaneClients();
+		logger.info(String.format("initOctaneClients took %s secs",	((System.currentTimeMillis() - startTime) / 1000)));
 	}
 
 	@Override
