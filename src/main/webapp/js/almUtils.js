896dfdeb8947e40a04ee103ee09d35819af5ae72
Dorin Bogdan <dorin.bogdan@microfocus.com>
2021-10-12 22:25:17 +0300

    US #687022 [Jenkins][Job Level Credentials] When changing to a new ALM Server the "Username" field should be empty

--- src/main/webapp/js/almUtils.js
+++ src/main/webapp/js/almUtils.js
@@ -4,69 +4,74 @@ if (window.NodeList && !NodeList.prototype.forEach) {
 const CredScope = {JOB : "JOB", SYSTEM : "SYSTEM"};
 
 function setupAlmCredentials() {
-	setTimeout(function() {
-		prepareTask()}, 200);
+	let divMain = null;
+	const selector = 'div[name="builder"][descriptorid="com.microfocus.application.automation.tools.run.RunFromAlmBuilder"]';
+	if (document.currentScript) {
+		divMain = document.currentScript.parentElement.closest(selector);
+	} else { // IE does not support document.currentScript
+		let divs = document.querySelectorAll(selector);
+		divMain = divs[divs.length-1];
+	}
+	setTimeout( function() {
+		prepareTask(divMain)}, 200);
 }
-function prepareTask() {
-	const credentialsScopeNodes = document.querySelectorAll('select[name="almCredentialsScope"]');
-	const isSSOEnabledNodes = document.querySelectorAll('input[type="checkbox"][name="runfromalm.isSSOEnabled"]');
-	const sysAlmUsernameNodes = document.querySelectorAll('div.sys-alm-username');
-	const sysAlmClientIdNodes = document.querySelectorAll('div.sys-alm-client-id');
-	const jobAlmUsernameNodes = document.querySelectorAll('div.job-alm-username');
-	const jobAlmPwdNodes = document.querySelectorAll('div.job-alm-password');
-	const jobAlmCIDNodes = document.querySelectorAll('div.job-alm-client-id');
-	const jobAlmApiKeyNodes = document.querySelectorAll('div.job-alm-secret');
+function prepareTask(divMain) {
+	const lstServerName = divMain.querySelector('select.alm-server-name');
+	const lstCredentialsScope = divMain.querySelector('select[name="almCredentialsScope"]');
+	const chkSsoEnabled = divMain.querySelector('input[type="checkbox"][name="runfromalm.isSSOEnabled"]');
+	const divSysAlmUsername = divMain.querySelector('div.sys-alm-username');
+	const divSysAlmClientId = divMain.querySelector('div.sys-alm-client-id');
+	const divJobAlmUsername = divMain.querySelector('div.job-alm-username');
+	const divJobAlmPwd = divMain.querySelector('div.job-alm-password');
+	const divJobAlmClientId = divMain.querySelector('div.job-alm-client-id');
+	const divJobAlmSecret = divMain.querySelector('div.job-alm-secret');
 
-	isSSOEnabledNodes.forEach(function(checkbox, index) {
-		selectCredentialsType(checkbox.checked, index, credentialsScopeNodes[index].value);
-		if (typeof checkbox.onclick !== "function") {
-			checkbox.onclick = function() {
-				selectCredentialsType(this.checked, index, credentialsScopeNodes[index].value);
-			}
-		}
-	});
-	credentialsScopeNodes.forEach(function(dropdown, idx) {
-		credentialsScopeChanged(dropdown.value, idx);
-		if (typeof dropdown.onchange !== "function") {
-			dropdown.onchange = function() {
-				credentialsScopeChanged(this.value, idx);
-			}
-		}
-	});
+	selectCredentialsType();
+	if (typeof chkSsoEnabled.onclick !== "function") {
+		chkSsoEnabled.onclick = selectCredentialsType;
+	}
+	if (typeof lstCredentialsScope.onchange !== "function") {
+		lstCredentialsScope.onchange = selectCredentialsType;
+	}
+	if (typeof lstServerName.onchange !== "function") {
+		lstServerName.onchange = resetCredentials;
+	}
+	function selectCredentialsType() {
+		const isJobScope = lstCredentialsScope.value === CredScope.JOB;
+		const isSSO = chkSsoEnabled.checked;
 
-	function selectCredentialsType(isSSO, idx, scope) {
-		const isJobScope = scope === CredScope.JOB;
-		const jobAlmUserNode = jobAlmUsernameNodes[idx];
-		const jobAlmPwdNode = jobAlmPwdNodes[idx];
-		const jobAlmCIDNode = jobAlmCIDNodes[idx];
-		const jobAlmApiKeyNode = jobAlmApiKeyNodes[idx];
-		const sysAlmUserNode = sysAlmUsernameNodes[idx];
-		const sysAlmCIDNode = sysAlmClientIdNodes[idx];
 		if (isJobScope) {
-			sysAlmUserNode.querySelector("select").name += "_x";
-			sysAlmCIDNode.querySelector("select").name += "_x";
-			sysAlmUsernameNodes[idx].style.display = sysAlmClientIdNodes[idx].style.display = "none";
-			jobAlmUserNode.querySelector("input").name = "almUserName";
-			jobAlmPwdNode.querySelector("input").name = "almPassword";
-			jobAlmCIDNode.querySelector("input").name = "almClientID";
-			jobAlmApiKeyNode.querySelector("input").name = "almApiKey";
-			jobAlmUsernameNodes[idx].style.display = jobAlmPwdNodes[idx].style.display = isSSO ? "none" : "block";
-			jobAlmCIDNodes[idx].style.display = jobAlmApiKeyNodes[idx].style.display = isSSO ? "block" : "none";
+			[divSysAlmUsername, divSysAlmClientId].forEach(function(div) { div.querySelector("select").name += "_x"; div.style.display = "none"; });
+			divJobAlmUsername.querySelector("input").name = "almUserName";
+			divJobAlmPwd.querySelector("input").name = "almPassword";
+			divJobAlmClientId.querySelector("input").name = "almClientID";
+			divJobAlmSecret.querySelector("input").name = "almApiKey";
+			divJobAlmUsername.style.display = divJobAlmPwd.style.display = isSSO ? "none" : "block";
+			divJobAlmClientId.style.display = divJobAlmSecret.style.display = isSSO ? "block" : "none";
 		} else {
-			sysAlmUserNode.querySelector("select").name = "almUserName";
-			sysAlmCIDNode.querySelector("select").name = "almClientID";
-			jobAlmUserNode.querySelector("input").name += "_x";
-			jobAlmCIDNode.querySelector("input").name += "_x";
-			jobAlmPwdNode.querySelector("input").name += "_x";
-			jobAlmApiKeyNode.querySelector("input").name += "_x";
-			jobAlmUsernameNodes[idx].style.display = jobAlmPwdNodes[idx].style.display = jobAlmCIDNodes[idx].style.display = jobAlmApiKeyNodes[idx].style.display = "none";
-			sysAlmUsernameNodes[idx].style.display = isSSO ? "none" : "block";
-			sysAlmClientIdNodes[idx].style.display = isSSO ? "block" : "none";
+			divSysAlmUsername.querySelector("select").name = "almUserName";
+			divSysAlmClientId.querySelector("select").name = "almClientID";
+			[divJobAlmUsername, divJobAlmPwd, divJobAlmClientId, divJobAlmSecret].forEach(function(div) { div.querySelector("input").name += "_x"; div.style.display = "none"; });
+			divSysAlmUsername.style.display = isSSO ? "none" : "block";
+			divSysAlmClientId.style.display = isSSO ? "block" : "none";
 		}
 	}
-
-	function credentialsScopeChanged(scope, idx) {
-		const isSSO = isSSOEnabledNodes[idx].checked;
-		selectCredentialsType(isSSO, idx, scope);
+	function resetCredentials() {
+		let evt;
+		try {
+			evt = new UIEvent("change", { "view": window, "bubbles": false, "cancelable": true });
+		} catch(e) { // IE does not support UIEvent constructor
+			evt = document.createEvent("UIEvent");
+			evt.initUIEvent("change", false, true, window, 1);
+		}
+		[divJobAlmSecret, divJobAlmPwd].forEach(function(div) {
+			const btnUpdate = div.querySelector("input.hidden-password-update-btn");
+			btnUpdate && btnUpdate.click();
+		});
+		[divJobAlmClientId, divJobAlmUsername, divJobAlmSecret, divJobAlmPwd].forEach(function(div) {
+			const input = div.querySelector("input");
+			input.value = "";
+			input.dispatchEvent(evt);
+		});
 	}
 }
