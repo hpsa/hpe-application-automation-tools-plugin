122a5634c60ddef3f138946d8cc89ab7236c2a92
Dorin Bogdan <dorin.bogdan@microfocus.com>
2022-10-19 14:46:59 +0300

    US-885047 [Jenkins] Implement Password type for Table of Parameters

--- HpToolsLauncher/Properties/Resources.Designer.cs
+++ HpToolsLauncher/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace HpToolsLauncher.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -537,6 +537,15 @@ namespace HpToolsLauncher.Properties {
         internal static string GeneralParameterUsage {
             get {
                 return ResourceManager.GetString("GeneralParameterUsage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Using parameter {0} = **********.
+        /// </summary>
+        internal static string GeneralParameterUsageMask {
+            get {
+                return ResourceManager.GetString("GeneralParameterUsageMask", resourceCulture);
             }
         }
         
